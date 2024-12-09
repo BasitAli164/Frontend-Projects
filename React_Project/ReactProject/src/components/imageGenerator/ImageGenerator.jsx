3b3739e97fb8add7caf51fce0692f240ccbe37ad
Basit Ali <opoali911@gmail.com>
2024-12-08 21:14:36 -0800

    the UI of the project is ready

--- React_Project/ReactProject/src/components/imageGenerator/ImageGenerator.jsx
+++ React_Project/ReactProject/src/components/imageGenerator/ImageGenerator.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './ImageGenerator.css';
 import defaultImage from '../../assets/image/aboutUs/about2.jpg'
 
 const ImageGenerator = () => {
+    const [imageUrl,setImageUrl]=useState("/");
+    let inputRef=useRef(null)
+
+    const imageGenerator=async()=>{
+        if(inputRef.current.value===""){
+            return 0;
+        }
+    }
   return (
     <div className='ai-image-generator'>
       <div className='header'>AI image <span>Generator</span></div>
       <div className='img-loading'>
-        <div className='image'><img src={defaultImage} alt="" width={300} /></div>
+        <div className='image'><img src={imageUrl==='/'?defaultImage:imageUrl} alt="" /></div>
+      </div>
+      <div className="searchBox">
+        <input type="text" ref={inputRef} className="search-input" placeholder='Describe What You Want To See ' />
+        <div className="generate-btn">Generate </div>
       </div>
     </div>
   )
