e23aa7f385f2af578e2696448978f3888c42e871
Basit Ali <opoali911@gmail.com>
2024-12-13 23:43:23 -0800

    working on beuatiful desing of form page

--- React_Project/ReactProject2/src/Pages/Form/Form.jsx
+++ React_Project/ReactProject2/src/Pages/Form/Form.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Form.css'
 import Input from '../../components/InputField/Input'
 
 const Form = () => {
+    const[formData,setFormData]=useState({
+        username:''
+    })
+    const handleChange=(e)=>{
+        const {name,type,value}=e.target;
+        const valueType=type==="text"||"email"||"password"?value:null;
+        setFormData({
+            ...formData,
+            [name]:valueType,
+        })
+    }
   return (
     <>
     <div className="container">
@@ -12,7 +23,19 @@ const Form = () => {
         <div className="underline"></div>
         <div className="form">
             <form >
-                {/* <Input/> */}
+                <Input
+                labelText="Username: "
+                inputType="text"
+                inputName="username"
+                inputId="username"
+                inputTitle="Username have at least 3 character"
+                inputPlaceholder="Enter the username"
+                inputMinLenght={3}
+                inputMaxLength={100}
+                inputValue={formData.username}
+                inputFunction={handleChange}
+                />
+                
                 
 
 
