0963f826a71863332653477dd55e8a5976c52fa8
Basit Ali <opoali911@gmail.com>
2024-12-20 12:09:10 -0800

    add more picture in custom slider

--- HTML_CSS_JS/Project-8/index.js
+++ HTML_CSS_JS/Project-8/index.js
@@ -1,16 +1,20 @@
 const slides=document.querySelectorAll('.slider');
 console.log(slides)
 let counter=0;
+let heading=document.querySelector("#head")
 slides.forEach((item,index)=>{
     item.style.left=`${index*100}%`
 })
 
+
 const goPrev=()=>{
     counter--;
+    heading.innerHTML=counter
     slideImage()
 }
 const goNext=()=>{
     counter++;
+    heading.innerHTML=counter
     slideImage()
 }
 const slideImage=()=>{
@@ -18,3 +22,4 @@ const slideImage=()=>{
         item.style.transform=`translateX(-${counter*100}%)`
     })
 }
+console.log(heading)
