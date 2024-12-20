424dde5373a82dc2bee23fcc296a84ac924c9b53
Basit Ali <opoali911@gmail.com>
2024-12-20 12:22:57 -0800

    finall commit in slider project

--- HTML_CSS_JS/Project-8/index.js
+++ HTML_CSS_JS/Project-8/index.js
@@ -1,33 +1,30 @@
-const slides=document.querySelectorAll('.slider');
+const slides = document.querySelectorAll('.slider');
 console.log(slides)
-let counter=0;
-let heading=document.querySelector("#head")
-slides.forEach((item,index)=>{
-    item.style.top=`${index*100}%`
+let counter = 0;
+let heading = document.querySelector("#head")
+slides.forEach((item, index) => {
+    item.style.top = `${index * 100}%`
 })
 
 
-const goPrev=()=>{
-    if(counter<slides.length)
-        {
-        console.log(`couter is: ${counter} and slides lenght is: ${slides.length}`)
-       
+const goPrev = () => {
+    if (counter < slides.length) {
+
         counter--;
-        heading.innerHTML=counter
+        heading.innerHTML = counter
         slideImage()
     }
 }
-const goNext=()=>{
-    heading.innerHTML=counter
-    if(counter<slides.length-1){
-        console.log(`couter is: ${counter} and slides lenght is: ${slides.length}`)
-    counter++;
-    slideImage()
+const goNext = () => {
+    heading.innerHTML = counter
+    if (counter < slides.length - 1) {
+
+        counter++;
+        slideImage()
     }
 }
-const slideImage=()=>{
-    slides.forEach((item)=>{
-        item.style.transform=`translateY(-${counter*100}%)`
+const slideImage = () => {
+    slides.forEach((item) => {
+        item.style.transform = `translateY(-${counter * 100}%)`
     })
 }
-console.log(heading)
