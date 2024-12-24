0d81038bad7342498d04d727404d7708d24516f4
Basit Ali <opoali911@gmail.com>
2024-12-24 12:12:06 -0800

    make a note app

--- HTML_CSS_JS/Porject-10/index.js
+++ HTML_CSS_JS/Porject-10/index.js
@@ -8,16 +8,35 @@ addBtn.addEventListener(
        addNote();
     }
 )
+
+const saveNote=()=>{
+    const notes=document.querySelectorAll(".note textarea");
+    const data=[];
+    notes.forEach((item)=>{
+        return data.push(item.value);
+    });
+    
+}
 const addNote=()=>{
     const note=document.createElement("div");
     note.classList.add("note");
     note.innerHTML=`
         <div class="tool">
-                <img src="delete.png" alt="" width="50">
-                <img src="edit.png" alt="" width="50">
+                <img class="delete" src="delete.png" alt="" width="50">
+                <img class="saveNotes" src="edit.png" alt="" width="50">
             </div>
             <textarea ></textarea>
-    `
+    `;
+    note.querySelector(".delete").addEventListener(
+        'click',
+        function(){
+            note.remove();
+        }
+    )
+    note.querySelector('.saveNotes').addEventListener(
+        "click",
+        saveNote()
+    )
     main.appendChild(note)
 
 }
