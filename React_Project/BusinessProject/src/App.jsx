577e2ebe9895f85b6e0e2f939940525a92f8a79b
Basit Ali <opoali911@gmail.com>
2025-01-02 01:18:29 +0500

    initial project setup almost finished

--- React_Project/BusinessProject/src/App.jsx
+++ React_Project/BusinessProject/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
      <h1>Business app</h1>
+     <Navbar/>
     </div>
   )
 }
