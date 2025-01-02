88702fdeeb3e7b77ff1c36138fddbbbb83363313
Basit Ali <opoali911@gmail.com>
2025-01-02 13:42:13 +0500

    start to make the header Part of the business app

--- React_Project/BusinessProject/src/App.jsx
+++ React_Project/BusinessProject/src/App.jsx
@@ -4,10 +4,9 @@ import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
-     <h1>Business app</h1>
+    <>
      <Navbar/>
-    </div>
+    </>
   )
 }
 
--- React_Project/BusinessProject/src/components/Navbar/Navbar.jsx
+++ React_Project/BusinessProject/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Navbar.css'
+import Logo from '../../assets/logo/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Nav Bar</h2>
-    </div>
+    <>
+    <header id='site_header'>
+      <div className="container">
+        <nav className="navbar" id='Navbar'>
+          <div className="navbar_brand">
+            <a href="#">
+              <img src={Logo}alt="Logo" />
+            </a>
+          </div>
+        </nav>
+      </div>
+
+    </header>
+    </>
   )
 }
 
