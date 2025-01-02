fee0d9a182a7ca0abe970a09cdc2870276440d09
Basit Ali <opoali911@gmail.com>
2025-01-02 14:00:45 +0500

    the menu are ready

--- React_Project/BusinessProject/src/components/Navbar/Navbar.jsx
+++ React_Project/BusinessProject/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,73 @@
 import React from 'react'
 import './Navbar.css'
 import Logo from '../../assets/logo/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
+  const menuIcon = <FontAwesomeIcon icon={faBars} />
   return (
     <>
-    <header id='site_header'>
-      <div className="container">
-        <nav className="navbar" id='Navbar'>
-          <div className="navbar_brand">
-            <a href="#">
-              <img src={Logo}alt="Logo" />
-            </a>
-          </div>
-        </nav>
-      </div>
+      <header id='site_header'>
+        <div className="container">
+          <nav className="navbar" id='Navbar'>
+            <div className="navbar_brand">
+              <a href="#">
+                <img src={Logo} alt="Logo" />
+              </a>
+            </div>
+            <div className="navbar_Toggler">{menuIcon}</div>
+            <div className="menu_items">
+              <ul>
+                <li> <Link
+                  activeClass="active"
+                  to="home"
+                  spy={true}
+                  smooth={true}
 
-    </header>
+                >
+                  Home
+                </Link></li>
+                <li> <Link
+                 to="about"
+                  spy={true}
+                  smooth={true}
+
+                >
+                  About Us
+                </Link></li>
+                <li> <Link
+                  to="Services"
+                  spy={true}
+                  smooth={true}
+
+                >
+                  Services
+                </Link></li>
+                <li> <Link
+                  to="blo"
+                  spy={true}
+                  smooth={true}
+
+                >
+                  Blog
+                </Link></li>
+                <li> <Link
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+
+                >
+                  Contact Us
+                </Link></li>
+               
+              </ul>
+            </div>
+          </nav>
+        </div>
+
+      </header>
     </>
   )
 }
