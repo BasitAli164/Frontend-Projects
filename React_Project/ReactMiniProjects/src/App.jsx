4e738e04c26e5d355869cba8a73f9a00e608f248
Basit Ali <opoali911@gmail.com>
2025-01-02 16:16:58 +0500

    Second mini project start

--- React_Project/ReactMiniProjects/src/App.jsx
+++ React_Project/ReactMiniProjects/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import Form from './Pages/Form/Form'
+import Form from './Project1/Pages/Form/Form'
 
 const App = () => {
   return (
     <>
+
+    {/*1st Mini Project*/}
     <Form/>
+    
+
     </>
   )
 }
