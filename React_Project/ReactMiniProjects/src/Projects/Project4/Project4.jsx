7f2f3b1d20f9e6c1f9c8fcc28011b808c21dfef3
Basit Ali <opoali911@gmail.com>
2025-01-04 19:31:11 +0500

    structre is ready

--- React_Project/ReactMiniProjects/src/Projects/Project4/Project4.jsx
+++ React_Project/ReactMiniProjects/src/Projects/Project4/Project4.jsx
@@ -3,7 +3,40 @@ import React from 'react'
 const Project4 = () => {
   return (
     <>
-      <h1>Project 4</h1>
+    <main>
+        <section className="slides">
+            <div className="slide">
+                <div className="content">
+                    <h1>Slide-1</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis fugit iure tempore, tenetur nobis perferendis quos quam excepturi ipsum totam nam eaque corrupti sit dicta quo dolorem delectus obcaecati deleniti.</p>
+                </div>
+            </div>
+            <div className="slide">
+                <div className="content">
+                    <h1>Slide-2</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis fugit iure tempore, tenetur nobis perferendis quos quam excepturi ipsum totam nam eaque corrupti sit dicta quo dolorem delectus obcaecati deleniti.</p>
+                </div>
+            </div>
+            <div className="slide">
+                <div className="content">
+                    <h1>Slide-3</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis fugit iure tempore, tenetur nobis perferendis quos quam excepturi ipsum totam nam eaque corrupti sit dicta quo dolorem delectus obcaecati deleniti.</p>
+                </div>
+            </div>
+            <div className="slide">
+                <div className="content">
+                    <h1>Slide-4</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis fugit iure tempore, tenetur nobis perferendis quos quam excepturi ipsum totam nam eaque corrupti sit dicta quo dolorem delectus obcaecati deleniti.</p>
+                </div>
+            </div>
+            <div className="slide">
+                <div className="content">
+                    <h1>Slide-5</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis fugit iure tempore, tenetur nobis perferendis quos quam excepturi ipsum totam nam eaque corrupti sit dicta quo dolorem delectus obcaecati deleniti.</p>
+                </div>
+            </div>
+        </section>
+    </main>
     </>
   )
 }
