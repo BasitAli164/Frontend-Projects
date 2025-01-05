d71567f18054056f98d3fce4aba8edeea5d65fdf
Basit Ali <opoali911@gmail.com>
2025-01-05 22:25:53 +0500

    Serive section is start

--- React_Project/BusinessProject/src/App.jsx
+++ React_Project/BusinessProject/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Service from './components/Service/Service'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
      <Navbar/>
      <Home/>
      <About/>
+     <Service/>
     </>
   )
 }
