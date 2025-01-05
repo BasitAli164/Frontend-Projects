50ab81fc3443070225a6e662e8db98dd711e37dc
Basit Ali <opoali911@gmail.com>
2025-01-05 22:54:07 +0500

    use the icons properly

--- React_Project/BusinessProject/src/components/Service/Service.jsx
+++ React_Project/BusinessProject/src/components/Service/Service.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Service.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faLabtopCode,faChartBar,faCopy,faMarker,faUserGear,faCoins} from "@fortawesome/free-solid-svg-icons"
+import {faLaptopCode,faChartBar,faCopy,faMarker,faUserGear,faCoins} from "@fortawesome/free-solid-svg-icons"
 
 const Service = () => {
-    const labtopIcon=<FontAwesomeIcon icon={faLabtopCode}/>
+    const labtopIcon=<FontAwesomeIcon icon={faLaptopCode}/>
     const chartIcon=<FontAwesomeIcon icon={faChartBar}/>
     const copyIcon=<FontAwesomeIcon icon={faCopy}/>
     const markerIcon=<FontAwesomeIcon icon={faMarker}/>
@@ -20,8 +20,67 @@ const Service = () => {
             </div>
             <div className="service_wrapper">
                 <div className="service_box">
-                    <div className="service_icon blue_icon">
-
+                    <div className="service_icon blue_icon">{labtopIcon}</div>
+                    <h4 className="number">01</h4>
+                    <div className="service_content">
+                        <h5>Cloud Computing </h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugit nulla delectus?</p>
+                        <a href="#" className='read'>Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div className="service_wrapper">
+                <div className="service_box">
+                    <div className="service_icon blue_icon">{chartIcon}</div>
+                    <h4 className="number">02</h4>
+                    <div className="service_content">
+                        <h5>Business Strategy  </h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugit nulla delectus?</p>
+                        <a href="#" className='read'>Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div className="service_wrapper">
+                <div className="service_box">
+                    <div className="service_icon blue_icon">{copyIcon}</div>
+                    <h4 className="number">03</h4>
+                    <div className="service_content">
+                        <h5>Report Analysis</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugit nulla delectus?</p>
+                        <a href="#" className='read'>Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div className="service_wrapper">
+                <div className="service_box">
+                    <div className="service_icon blue_icon">{markerIcon}</div>
+                    <h4 className="number">04</h4>
+                    <div className="service_content">
+                        <h5>Decision Maker </h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugit nulla delectus?</p>
+                        <a href="#" className='read'>Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div className="service_wrapper">
+                <div className="service_box">
+                    <div className="service_icon blue_icon">{userGearIcon}</div>
+                    <h4 className="number">05</h4>
+                    <div className="service_content">
+                        <h5>Customer Oriented </h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugit nulla delectus?</p>
+                        <a href="#" className='read'>Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div className="service_wrapper">
+                <div className="service_box">
+                    <div className="service_icon blue_icon">{coinIcon}</div>
+                    <h4 className="number">06</h4>
+                    <div className="service_content">
+                        <h5>Solution Focused  </h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fugit nulla delectus?</p>
+                        <a href="#" className='read'>Read More</a>
                     </div>
                 </div>
             </div>
