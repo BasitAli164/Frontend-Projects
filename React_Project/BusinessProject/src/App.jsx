197972218aed673c5dae89b8cfafd6fb5b8bb846
Basit Ali <opoali911@gmail.com>
2025-01-05 23:57:34 +0500

    start the blog section

--- React_Project/BusinessProject/src/App.jsx
+++ React_Project/BusinessProject/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Service from './components/Service/Service'
+import Blog from './components/Blog/Blog'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
      <Home/>
      <About/>
      <Service/>
+     <Blog/>
     </>
   )
 }
