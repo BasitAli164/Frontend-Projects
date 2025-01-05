b99a22b9363e1606047228020f7d768b3cb201e0
Basit Ali <opoali911@gmail.com>
2025-01-05 22:30:00 +0500

    start to make the service structure

--- React_Project/BusinessProject/src/components/Service/Service.jsx
+++ React_Project/BusinessProject/src/components/Service/Service.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <div>
-      
+    <>
+    <div className="services">
+        <div className="container">
+            <div className="title_heading">
+                <h3>What's Serivces We Are Offering to Our Customers</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi officia perspiciatis labore molestiae saepe aut.</p>
+            </div>
+            <div className="service_wrapper"></div>
+        </div>
     </div>
+      
+    </>
   )
 }
 
