c6938ae17b76247e353546ae32d491230d784fe2
Basit Ali <opoali911@gmail.com>
2025-01-06 16:00:18 +0500

    making the header part of the project

--- React_Project/SperingProject/src/components/Header/Header.jsx
+++ React_Project/SperingProject/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Header.css'
 import logo from '../../assets/images/logo.png'
+
 const Header = () => {
+    
     return (
         <>
             <header id='header'>
@@ -9,6 +11,14 @@ const Header = () => {
                     <div className="logo">
                         <img src={logo} alt="Logo" />
                         <h2>Spring</h2>
+                    </div>
+                </div>
+                <div className="menuItems">
+                    <div className="menuList">
+                        <ul>
+                            <li> </li>
+
+                        </ul>
                     </div>
                 </div>
             </header>
