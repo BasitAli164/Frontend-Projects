e2f039d21a50303c8084c97ee5cdaa6617446437
Basit Ali <opoali911@gmail.com>
2025-01-07 15:43:33 +0500

    the landing page is almost ready

--- React_Project/SperingProject/src/pages/Home/LandingPage.jsx
+++ React_Project/SperingProject/src/pages/Home/LandingPage.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import './LandingPage.css'
-import sliderImage from '../../assets/images/slider-img.png'
-import Button from '../../components/Button/Button'
+import React from 'react';
+import './LandingPage.css';
+import Button from '../../components/Button/Button';
+import sliderImage from '../../assets/images/slider-img.png';
+import prevIcon from '../../assets/images/prev.png';
+import nextIcon from '../../assets/images/next.png';
+
 const LandingPage = () => {
 
     return (
@@ -10,12 +13,21 @@ const LandingPage = () => {
                 <div className="leftPart">
                     <div className="headings">
                     <h1>You Can <br /> Hire Freelancer <br />Here</h1>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+                    <p>It is a long established fact that a reader will be distracted by the <br /> readable content of a page</p>
                    
                    <div className="btn">
                    <Button btnText="About Us" btnName="redish"/>
                     <Button btnText="About Us" />
 
+                   </div>
+
+                   <div className="landingIcons">
+                    <div className="prevIcon">
+                        <img src={prevIcon} alt="" />
+                    </div>
+                    <div className="nextIcon">
+                        <img src={nextIcon} alt="" />
+                    </div>
                    </div>
                    </div>
                 </div>
