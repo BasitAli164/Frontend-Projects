46defc2bba9262600d460dd47edd287a8408dafe
Basit Ali <opoali911@gmail.com>
2025-01-08 18:38:24 +0500

    almost complete the header

--- React_Project/SperingProject/src/components/Header/Header.jsx
+++ React_Project/SperingProject/src/components/Header/Header.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logoImage from "../../assets/images/logo.png"
 import serachIcon from '../../assets/images/search-icon.png'
 import menuIcon from '../../assets/images/menu.png'
 import {Link } from 'react-router-dom'
 
 const Header = () => {
+    const [toggle,setToggle]=useState(true);
+    const handleChanges=()=>{
+        setToggle(!toggle)
+    }
   return (
     <>
     <div className='w-full bg-slate-400 h-auto flex justify-between items-center'>
         <div>
-            <div className='flex justify-center items-center'>
-                <img className='w-12' src={logoImage} alt="Logo-Image" />
-                <h3 className='text-slate-100 text-sm md:text-lg font-bold'>Spering</h3>
-                </div>
+            <div className='ml-10 flex justify-center items-center'>
+                <img className='w-10 md:w-8' src={logoImage} alt="Logo-Image" />
+                <h3 className='text-slate-100 text-sm md:text-xl font-bold'>Spering</h3>
+        </div>
         </div>
         <div>
             <ul className='hidden sm:flex'>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-5'><Link  className="md:text-xl"to='/'>Home</Link></li>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-5'><Link  className="md:text-xl"to='/about'>About</Link></li>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-5'><Link  className="md:text-xl"to='/work'>Work</Link></li>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-5' ><Link className="md:text-xl" to="/category">Category</Link></li>
+                <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link  className="md:text-lg"to='/'>Home</Link></li>
+                <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link  className="md:text-lg"to='/about'>About</Link></li>
+                <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link  className="md:text-lg"to='/work'>Work</Link></li>
+                <li className='mx-1 text-slate-200 text-[16px] md:mx-4' ><Link className="md:text-lg" to="/category">Category</Link></li>
             </ul>
         </div>
-        <div>
-            <div><button>Login</button></div>
-            <div><img src={serachIcon} alt="SearchIcon" /></div>
+        <div className='hidden sm:flex ml-42 space-x-5'>
+            <div><button className='text-lg text-slate-100 bg-transparent '>Login</button></div>
+            <div><img className='sm:mt-2 ' src={serachIcon} alt="SearchIcon" /></div>
         </div>
-        <div>
-            <div><img src={menuIcon} alt="menuIcon" /></div>
-            <div><span>X</span></div>
+        <div className='mr-5'>
+            {
+                toggle==true?
+                <div><img onClick={handleChanges} src={menuIcon} alt="menuIcon" /></div>
+                :
+                <div><span className='text-xl text-slate-100 font-bold' onClick={handleChanges}>X</span></div>
+
+            }
         </div>
     </div>
     </>
