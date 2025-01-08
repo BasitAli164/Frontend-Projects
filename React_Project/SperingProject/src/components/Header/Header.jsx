5a2066124d9557f4c782f6c827591fe924f8bb96
Basit Ali <opoali911@gmail.com>
2025-01-08 19:12:48 +0500

    the header component is almost complete

--- React_Project/SperingProject/src/components/Header/Header.jsx
+++ React_Project/SperingProject/src/components/Header/Header.jsx
@@ -2,46 +2,54 @@ import React, { useState } from 'react'
 import logoImage from "../../assets/images/logo.png"
 import serachIcon from '../../assets/images/search-icon.png'
 import menuIcon from '../../assets/images/menu.png'
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-    const [toggle,setToggle]=useState(true);
-    const handleChanges=()=>{
+    const [toggle, setToggle] = useState(true);
+    const handleChanges = () => {
         setToggle(!toggle)
     }
-  return (
-    <>
-    <div className='w-full bg-slate-400 h-auto flex justify-between items-center'>
-        <div>
-            <div className='ml-10 flex justify-center items-center'>
-                <img className='w-10 md:w-8' src={logoImage} alt="Logo-Image" />
-                <h3 className='text-slate-100 text-sm md:text-xl font-bold'>Spering</h3>
-        </div>
-        </div>
-        <div>
-            <ul className='hidden sm:flex'>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link  className="md:text-lg"to='/'>Home</Link></li>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link  className="md:text-lg"to='/about'>About</Link></li>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link  className="md:text-lg"to='/work'>Work</Link></li>
-                <li className='mx-1 text-slate-200 text-[16px] md:mx-4' ><Link className="md:text-lg" to="/category">Category</Link></li>
-            </ul>
-        </div>
-        <div className='hidden sm:flex ml-42 space-x-5'>
-            <div><button className='text-lg text-slate-100 bg-transparent '>Login</button></div>
-            <div><img className='sm:mt-2 ' src={serachIcon} alt="SearchIcon" /></div>
-        </div>
-        <div className='mr-5'>
-            {
-                toggle==true?
-                <div><img onClick={handleChanges} src={menuIcon} alt="menuIcon" /></div>
-                :
-                <div><span className='text-xl text-slate-100 font-bold' onClick={handleChanges}>X</span></div>
+    return (
+        <>
+            <div className='w-full bg-slate-400 h-auto flex justify-between items-center'>
+                <div>
+                    <div className='ml-10 flex justify-center items-center'>
+                        <img className='w-6 sm:w-8' src={logoImage} alt="Logo-Image" />
+                        <h3 className='text-slate-100 text-sm sm:text-lg font-bold'>Spering</h3>
+                    </div>
+                </div>
+               {
+                toggle==false?
+                <>
+                 <div>
+                    <ul className='hidden sm:flex'>
+                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/'>Home</Link></li>
+                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/about'>About</Link></li>
+                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/work'>Work</Link></li>
+                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4' ><Link className="md:text-lg" to="/category">Category</Link></li>
+                    </ul>
+                </div>
+                <div className='hidden sm:flex ml-42 space-x-5'>
+                    <div><button className='text-lg text-slate-100 bg-transparent '>Login</button></div>
+                    <div><img className='sm:mt-2 ' src={serachIcon} alt="SearchIcon" /></div>
+                </div>
+                </>:
+                <></>
+               }
+                <div className='mr-5'>
+                    {
+                        toggle == true ?
+                            <div><img onClick={handleChanges} src={menuIcon} alt="menuIcon" /></div>
+                            :
+                            <div>
+                                <span className='text-xl text-slate-100 font-bold cursor-default' onClick={handleChanges}>X</span>
+                            </div>
 
-            }
-        </div>
-    </div>
-    </>
-  )
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Header
