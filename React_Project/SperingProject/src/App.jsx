6827f6530ac4512c3c664ca6de155e81b9268e9c
Basit Ali <opoali911@gmail.com>
2025-01-08 20:04:15 +0500

    modify in the header

--- React_Project/SperingProject/src/App.jsx
+++ React_Project/SperingProject/src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from './components/Header/Header'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Work from './Pages//Work/Work'
+import Category from './Pages/Category/Category'
 const App = () => {
   return (
     <>
     <Router>
   <Header/>
     <Routes>
-      <Route to="/" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/work' element={<Work/>}  />
+      <Route path='/category' element={<Category/>} />
     </Routes>
 
     </Router>
--- React_Project/SperingProject/src/components/Header/Header.jsx
+++ React_Project/SperingProject/src/components/Header/Header.jsx
@@ -15,28 +15,28 @@ const Header = () => {
             <div className='w-full bg-slate-400 h-auto flex justify-between items-center'>
                 <div>
                     <div className='ml-10 flex justify-center items-center'>
-                        <img className='w-6 sm:w-8' src={logoImage} alt="Logo-Image" />
+                        <img className='w-6 sm:w-8 py-2' src={logoImage} alt="Logo-Image" />
                         <h3 className='text-slate-100 text-sm sm:text-lg font-bold'>Spering</h3>
                     </div>
                 </div>
-               {
-                toggle==false?
-                <>
-                 <div>
-                    <ul className='hidden sm:flex'>
-                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/'>Home</Link></li>
-                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/about'>About</Link></li>
-                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/work'>Work</Link></li>
-                        <li className='mx-1 text-slate-200 text-[16px] md:mx-4' ><Link className="md:text-lg" to="/category">Category</Link></li>
-                    </ul>
-                </div>
-                <div className='hidden sm:flex ml-42 space-x-5'>
-                    <div><button className='text-lg text-slate-100 bg-transparent '>Login</button></div>
-                    <div><img className='sm:mt-2 ' src={serachIcon} alt="SearchIcon" /></div>
-                </div>
-                </>:
-                <></>
-               }
+                {
+                    toggle == false ?
+                        <>
+                            <div>
+                                <ul className='hidden sm:flex'>
+                                    <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/'>Home</Link></li>
+                                    <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/about'>About</Link></li>
+                                    <li className='mx-1 text-slate-200 text-[16px] md:mx-4'><Link className="md:text-lg" to='/work'>Work</Link></li>
+                                    <li className='mx-1 text-slate-200 text-[16px] md:mx-4' ><Link className="md:text-lg" to="/category">Category</Link></li>
+                                </ul>
+                            </div>
+                            <div className='hidden sm:flex ml-42 space-x-5'>
+                                <div><button className='text-lg text-slate-100 bg-transparent '>Login</button></div>
+                                <div><img className='sm:mt-2 ' src={serachIcon} alt="SearchIcon" /></div>
+                            </div>
+                        </> :
+                        <></>
+                }
                 <div className='mr-5'>
                     {
                         toggle == true ?
@@ -45,7 +45,7 @@ const Header = () => {
                             </div>
                             :
                             <div>
-                                <span className='hidden sm:block text-xl text-slate-100 font-bold cursor-default' onClick={handleChanges}>X</span>
+                                <span className='hidden sm:block text-3xl text-slate-100 font-bold cursor-default' onClick={handleChanges}>X</span>
                                 <img className='sm:hidden' onClick={handleChanges} src={menuIcon} alt="menuIcon" />
 
                             </div>
@@ -53,19 +53,29 @@ const Header = () => {
                     }
                 </div>
             </div>
-           {
-            toggle==true?
-            <>
-                    <ul className='flex flex-col justify-center items-center sm:hidden'>
-                        <li className='my-2 text-slate-950 text-[16px] '><Link className="md:text-lg" to='/'>Home</Link></li>
-                        <li className='my-2 text-slate-950 text-[16px] '><Link className="md:text-lg" to='/about'>About</Link></li>
-                        <li className='my-2 text-slate-950 text-[16px] '><Link className="md:text-lg" to='/work'>Work</Link></li>
-                        <li className='my-2 text-slate-950 text-[16px] ' ><Link className="md:text-lg" to="/category">Category</Link></li>
-                    </ul>
-           
-            </>:
-            ""
-           }
+            {
+                toggle == true ?
+                    <>
+
+                        <ul className='flex flex-col justify-center items-center sm:hidden'>
+                            <li className='my-2 text-slate-950 text-[16px] '>
+                                <Link className="md:text-lg" to='/'>Home</Link>
+                            </li>
+                            <li className='my-2 text-slate-950 text-[16px] '>
+                                <Link className="md:text-lg" to='/about'>About</Link>
+                            </li>
+                            <li className='my-2 text-slate-950 text-[16px] '>
+                                <Link className="md:text-lg" to='/work'>Work</Link>
+                            </li>
+                            <li className='my-2 text-slate-950 text-[16px] ' >
+                                <Link className="md:text-lg" to="/category">Category</Link>
+                            </li>
+                        </ul>
+
+
+                    </> :
+                    ""
+            }
         </>
     )
 }
