edec6bf3ffa4c356ced4c464b703a6faf60d472c
Basit Ali <opoali911@gmail.com>
2025-01-10 00:38:57 +0500

    working on the about pate

--- React_Project/SperingProject/src/Pages/About/About.jsx
+++ React_Project/SperingProject/src/Pages/About/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
     <>
     <section className='bg-white w-full h-screen '>
       <div className='w-full h-screen flex flex-col   sm:flex-row '>
-        <div className='w-full h-1/2 flex items-center justify-center overflow-hidden o sm:w-[45%]'>
-          <img className='w-[50%]' src={experienceImage} alt="Experience_Image" />
+        <div className='w-full h-screen  flex items-center justify-center overflow-hidden bg-cover bg-center  sm:w-[45%]' style={{backgroundImage:`url(${experienceImage})`}}>
+          {/* <img className='w-[50%]' src={experienceImage} alt="Experience_Image" /> */}
         </div>
-        <div className='w-full sm:w-[55%]'>
-        <h2 className='text-black'>Best Experinced Freelancer Here</h2>
-          <p className='text-black'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
+        <div className='w-full flex flex-col justify-center  items-center sm:w-[55%]'>
+        <h2 className='text-black text-4xl font-bold'>Best Experinced Freelancer Here</h2>
+          <p className='text-black text-xl bg-red-400'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
           <div>
             <Button1 btnText="Read More" />
             <Button2 btnText= "Hire"/>
