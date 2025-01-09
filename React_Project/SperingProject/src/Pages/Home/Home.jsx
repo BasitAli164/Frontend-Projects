2503bf00db25bfc9a391bc665fbf83c643aa3597
Basit Ali <opoali911@gmail.com>
2025-01-09 18:29:20 +0500

    almost complete the left part of home with mobile responsive

--- React_Project/SperingProject/src/Pages/Home/Home.jsx
+++ React_Project/SperingProject/src/Pages/Home/Home.jsx
@@ -4,22 +4,22 @@ import {Button1,Button2} from '../../components/Header/Button/Button'
 const Home = () => {
   return (
     <>
-    <main className='w-full h-screen flex flex-col  justify-between  md:flex-row '>
-    <section className='w-full h-[50%]  bg-[#1a2e35] flex justify-center items-center md:w-[60%] md:h-screen  md:items-center md:justify-center'>
+    <main className='w-full h-screen flex flex-col  justify-between  sm:flex-row '>
+    <section className='w-full h-[50%]  bg-[#1a2e35] flex justify-center items-center sm:w-[60%] sm:h-screen  sm:items-center sm:justify-center'>
         <div className='flex flex-col justify-center items-center gap-2 text-center'>
         <div>
-          <h2 className='text-3xl font-bold leading-8 md:text-4xl md:font-medium md:leading-10'>You Can Hire Freelancer Here</h2>
+          <h2 className='text-3xl font-bold leading-8 sm:text-4xl sm:font-medium sm:leading-10'>You Can Hire Freelancer Here</h2>
         </div>
         <div>
           <p className='text-sm leading-relaxed'>It is a long established fact that a reader will be distracted by the readable content of a page</p>
         </div>
-        <div className='flex flex-col mt-3'>
+        <div className='space-x-8  '>
           <Button1 btnText="About Us" />
           <Button2 btnText="Get a job"/>
         </div>
         </div>
       </section>
-      <section className='w-full h-[50%] bg-[#1cbbb4] md:w-[40%] md:h-screen md:flex md:items-center md:justify-center'>
+      <section className='w-full h-[50%] bg-[#1cbbb4] sm:w-[40%] sm:h-screen sm:flex sm:items-center sm:justify-center'>
         <div>Image</div>
         <div>counter</div>
       </section>
--- React_Project/SperingProject/src/components/Header/Button/Button.jsx
+++ React_Project/SperingProject/src/components/Header/Button/Button.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Button1 = ({btnText}) => {
   return (
     <>
-        <button className=' px-10 py-2 text-lg text-slate-100 bg-red-500 
-        hover:bg-transparent hover:text-red-500 hover:border hover:border-solid hover:border-red-400'>{btnText}</button>          
+        <button className='px-4 py-2 text-md md:m-3 md:px-10 md:py-2 md:text-lg text-slate-100 bg-red-500 hover:bg-transparent hover:text-red-500 hover:border hover:border-solid hover:border-red-400 '>{btnText}</button>          
     </>
   )
 }
@@ -13,7 +12,7 @@ const Button2=({btnText})=>{
     return(
 
     <>
-        <button className=' px-10 py-2 text-lg text-slate-950 bg-slate-50 
+        <button className='px-4 py-2 text-md mt-4 md:m-3 md:px-10 md:py-2 md:text-lg text-slate-950 bg-slate-50 
         hover:bg-transparent hover:text-slate-50 hover:border hover:border-solid hover:border-slate-50'>{btnText}</button>          
     </>
 
