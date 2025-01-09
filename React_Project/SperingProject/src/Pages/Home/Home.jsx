61975b4f233ffa4078c9767745a139fbfc51994d
Basit Ali <opoali911@gmail.com>
2025-01-09 17:18:40 +0500

    left side almost complete

--- React_Project/SperingProject/src/Pages/Home/Home.jsx
+++ React_Project/SperingProject/src/Pages/Home/Home.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import {Button1,Button2} from '../../components/Button/Button'
+import {Button1,Button2} from '../../components/Header/Button/Button'
 
 const Home = () => {
   return (
     <>
     <main className='w-full h-screen flex flex-col  justify-between  md:flex-row '>
-    <section className='w-full h-[50%]  bg-[#1a2e35] justify-center items-center md:w-[60%] md:h-screen  '>
+    <section className='w-full h-[50%]  bg-[#1a2e35] flex justify-center items-center md:w-[60%] md:h-screen  md:items-center md:justify-center'>
+        <div className='flex flex-col justify-center items-center gap-2 text-center'>
         <div>
-        <div>
-          <h2>You Can Hire Freelancer Here</h2>
+          <h2 className='text-3xl font-bold leading-8 md:text-4xl md:font-medium md:leading-10'>You Can Hire Freelancer Here</h2>
         </div>
         <div>
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+          <p className='text-sm leading-relaxed'>It is a long established fact that a reader will be distracted by the readable content of a page</p>
         </div>
-        <div>
-          <Button1 btnText="About Us"/>
+        <div className='flex flex-col mt-3'>
+          <Button1 btnText="About Us" />
           <Button2 btnText="Get a job"/>
         </div>
         </div>
