8fef0865cecb53188663eea0ddbba736c27000ba
Basit Ali <opoali911@gmail.com>
2025-01-10 13:55:34 +0500

    now start the about section

--- React_Project/SperingProject/src/Pages/About/About.jsx
+++ React_Project/SperingProject/src/Pages/About/About.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Button1, Button2 } from '../../components/Header/Button/Button'
 
 const About = () => {
   return (
-    <div>
-        <h1>Hello from about</h1>
+    <>
+    <section>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+        <h2>Best Experinced Freelancer Here</h2>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as
+          <div>
+            <Button1 btnText="Read More" />
+            <Button2 btnText= "Hire"/>
+          </div>
+
+</p>
+        </div>
+      </div>
+    </section>
       
-    </div>
+    </>
   )
 }
 
