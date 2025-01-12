f1c450fcc5fb408e52ae1ec7ed6cf10495dba612
Basit Ali <opoali911@gmail.com>
2025-01-12 18:42:28 +0500

    the category section is rady

--- React_Project/SperingProject/src/Pages/Category/Category.jsx
+++ React_Project/SperingProject/src/Pages/Category/Category.jsx
@@ -2,29 +2,45 @@ import React from "react";
 import categoryImage1 from "../../assets/images/c1.png";
 import categoryImage2 from "../../assets/images/c2.png";
 import categoryImage3 from "../../assets/images/c3.png";
+import categoryImage4 from "../../assets/images/c4.png";
+import categoryImage5 from "../../assets/images/c5.png";
+import categoryImage6 from "../../assets/images/c6.png";
 
 const Category = () => {
   return (
     <>
-      <section className="w-full h-screen bg-[#1cbbb4] flex  justify-center">
-        <div className="flex flex-col  items-center w-full sm:w-[90%] border-2 bg-rose-500">
+      <section className="w-full  bg-[#1cbbb4] flex  justify-center">
+        <div className="flex flex-col  items-center w-full sm:w-[90%] ">
           <div className="mt-20 text-center">
-            <h1 className="text-3xl font-semibold border-b-2 border-slate-300">
+            <h1 className="text-5xl font-semibold ">
               Category
             </h1>
+            <div className="w-52 h-1 bg-slate-200 mt-5"></div>
           </div>
-          <div className="w-[80%] flex flex-wrap gap-28 justify-between items-center mt-16 bg-slate-400">
-            <div className="flex flex-col justify-center items-center bg-red-300">
+          <div className="w-full sm:w-[80%] flex flex-wrap gap-28 justify-center sm:justify-between items-center mt-10 mb-20">
+            <div className="flex flex-col justify-center items-center ">
               <img className="w-[50%]" src={categoryImage1} alt="" />
               <h3 className="mt-3 text-2xl font-semibold">Design & Arts</h3>
             </div>
-            <div className="flex flex-col justify-center items-center bg-red-300">
-              <img className="w-[50%]" src={categoryImage2} alt="" />
+            <div className="flex flex-col justify-center items-center ">
+              <img className="w-[40%]" src={categoryImage2} alt="" />
               <h3 className="mt-3 text-2xl font-semibold">Web Development</h3>
             </div>
-            <div className="flex flex-col justify-center items-center bg-red-300">
-              <img className="w-[90%]" src={categoryImage3} alt="" />
+            <div className="flex flex-col justify-center items-center ">
+              <img className="w-[50%]" src={categoryImage3} alt="" />
               <h3 className="mt-3 text-2xl font-semibold">SEO Markting</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center ">
+              <img className="w-[50%]" src={categoryImage4} alt="" />
+              <h3 className="mt-3 text-2xl font-semibold">Video Edting</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center ">
+              <img className="w-[50%]" src={categoryImage5} alt="" />
+              <h3 className="mt-3 text-2xl font-semibold">Logo Design</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center ">
+              <img className="w-[50%]" src={categoryImage6} alt="" />
+              <h3 className="mt-3 text-2xl font-semibold">Game Design</h3>
             </div>
           </div>
         </div>
