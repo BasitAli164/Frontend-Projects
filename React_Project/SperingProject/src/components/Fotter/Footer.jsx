3fcfce5cc7730f7bc18d1e1861cbf5089ae15582
Basit Ali <opoali911@gmail.com>
2025-01-12 16:59:13 +0500

    working on the responsiveness

--- React_Project/SperingProject/src/components/Fotter/Footer.jsx
+++ React_Project/SperingProject/src/components/Fotter/Footer.jsx
@@ -10,58 +10,61 @@ const Footer = () => {
   return (
     <>
     <footer className='w-full h-[100vh] flex flex-col items-center border-[3px] border-red-700 bg-[#1a2e35]'>
-      <section className='w-full flex flex-col justify-center gap-20 sm:max-w-[90%] xl:max-w-[90%] bg-red-200 mt-10'>
+      <section className='w-full h-[77vh]  flex flex-col justify-center gap-16 sm:max-w-[90%] xl:max-w-[90%] bg-red-200 mt-10'>
         <div className='flex flex-col sm:flex-row gap-3 justify-between items-center '>
           <div className='flex justify-center items-center gap-2'>
-            <img className='w-10 sm:w-14 xl:w-20' src={logoImage} alt="Logo-Icon" />
-            <h2 className='text-2xl sm:text-3xl font-bold xl:text-6xl'>Spering</h2>
+            <img className='w-10 sm:w-14 xl:w-14' src={logoImage} alt="Logo-Icon" />
+            <h2 className='text-2xl sm:text-3xl font-bold xl:text-5xl'>Spering</h2>
           </div>
           <div className='flex justify-center items-center gap-x-1 sm:flex-row '>
-            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={facebookIcon} alt="Facebook-Icon" />
-            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={twitterIcon} alt="Twitter-Icon" />
-            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={linkedinIcon} alt="LinkedIn-Icon" />
-            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={instagramIcon} alt="Instagram-Icon" />
-            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={youtubeIcon} alt="Youtube-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-10 ' src={facebookIcon} alt="Facebook-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-10 ' src={twitterIcon} alt="Twitter-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-10 ' src={linkedinIcon} alt="LinkedIn-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-10 ' src={instagramIcon} alt="Instagram-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-10 ' src={youtubeIcon} alt="Youtube-Icon" />
           </div>
         </div>
-        <div className='grid grid-rows-4 text-center gap-3 sm:grid-cols-4'>
-          <div>
-            <h2 className='text-2xl font-bold uppercase tracking-wider xl:text-4xl'>Useful Link</h2>
+        <div className='grid grid-rows-4 text-center gap-3 sm:grid-cols-4 bg-blue-600 h-[30vh]'>
+          <div className='bg-red-400'>
+            <h2 className='text-2xl font-bold uppercase tracking-wider md:text-xl xl:text-2xl'>Useful Link</h2>
             <ul>
-              <li className='text-lg xl:text-3xl'>
+              <li className='text-lg xl:text-xl'>
                 <Link to='/'>Home</Link>
               </li>
-              <li className='text-lg xl:text-3xl'>
+              <li className='text-lg xl:text-xl'>
                 <Link to='/about'>About</Link>
               </li>
-              <li className='text-lg xl:text-3xl'>
+              <li className='text-lg xl:text-xl'>
                 <Link to='/work'>Work</Link>
               </li>
-              <li className='text-lg xl:text-3xl'>
+              <li className='text-lg xl:text-xl'>
                 <Link to='/category'>Category</Link>
               </li>
             </ul>
           </div>
           <div>
-            <h2 className='text-2xl font-bold uppercase tracking-wider xl:text-4xl'>Office</h2>
-            <p className='text-lg text-center sm:text-justify sm:text-xl'>Readable content of a page when looking at its layoutreadable content of a page when looking at its layout</p>
+            <h2 className='text-2xl font-bold uppercase tracking-wider md:text-xl xl:text-2xl'>Office</h2>
+            <p className='text-lg text-center sm:text-justify sm:text-md'>Readable content of a page when looking at its layoutreadable content of a page when looking at its layout</p>
           </div>
           <div>
-          <h2 className='text-2xl font-bold uppercase tracking-wider xl:text-4xl'>Information</h2>
-          <p className='text-lg text-center sm:text-justify sm:text-xl'>Readable content of a page when looking at its layoutreadable content of a page when looking at its layout</p>
+          <h2 className='text-2xl font-bold uppercase tracking-wider md:text-xl xl:text-2xl'>Information</h2>
+          <p className='text-lg text-center sm:text-justify sm:text-md'>Readable content of a page when looking at its layoutreadable content of a page when looking at its layout</p>
           </div>
+          <div className='flex flex-col gap-y-4 sm:gap-2'>
+          <h2 className='text-2xl font-bold uppercase tracking-wider md:text-xl xl:text-2xl'>Newsletter</h2>
           <div>
-          <h2 className='text-2xl font-bold uppercase tracking-wider xl:text-4xl'>Newsletter</h2>
-          <div>
-            <input className='w-[80%] p-4' type="text" placeholder='Email' />
+            <input className='w-[80%] p-4 sm:p-1 xl:p-2 xl:w-[95%] xl:placeholder:text-xl' type="text" placeholder='Email' />
           </div>
+          <div className='mb-4'>
           <Button1 btnText="SUBSCRIBE"/>
 
           </div>
+          </div>
 
         </div>
+        <div></div>
       </section>
-      <section className='w-full text-center p-6 bg-white'>
+      <section className='w-full h-[15vh] text-center p-6 bg-white'>
         <p className='text-md sm:text-2xl text-black  tracking-wider '>© 2025 All Rights Reserved By Free Html Templates</p>
       </section>
     </footer>
--- React_Project/SperingProject/src/components/Header/Button/Button.jsx
+++ React_Project/SperingProject/src/components/Header/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button1 = ({btnText}) => {
   return (
     <>
-        <button className='px-4 py-2 text-md md:m-3 md:px-10 md:py-2 md:text-lg text-slate-100 bg-red-500 hover:bg-transparent hover:text-red-500 hover:border hover:border-solid hover:border-red-400 '>{btnText}</button>          
+        <button className='px-4 py-2 text-md md:m-3 md:px-6 lg:px-10 md:py-2 md:text-lg text-slate-100 bg-red-500 hover:bg-transparent hover:text-red-500 hover:border hover:border-solid hover:border-red-400 '>{btnText}</button>          
     </>
   )
 }
