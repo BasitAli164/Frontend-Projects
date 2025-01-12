8fbaa903d89b8b36cd7331b4798892ec8018715d
Basit Ali <opoali911@gmail.com>
2025-01-12 15:07:36 +0500

    the upside of the footer is almost ready

--- React_Project/SperingProject/src/components/Fotter/Footer.jsx
+++ React_Project/SperingProject/src/components/Fotter/Footer.jsx
@@ -8,21 +8,22 @@ import youtubeIcon from '../../assets/images/youtube.png'
 const Footer = () => {
   return (
     <>
-    <footer className='w-full flex flex-col items-center border-[3px] border-red-700'>
-      <section className='max-w-sm'>
-        <div>
-          <div>
-            <img src={logoImage} alt="Logo-Icon" />
-            <h2>Spering</h2>
+    <footer className='w-full h-[50vh] flex flex-col items-center border-[3px] border-red-700 bg-[#1a2e35]'>
+      <section className='w-full sm:max-w-[1170px] bg-red-200 mt-10'>
+        <div className='grid grid-rows-2 gap-y-3  sm:grid-cols-2 sm:gap-x-[400px] bg-red-300'>
+          <div className='flex justify-center items-center'>
+            <img className='w-10 sm:w-20' src={logoImage} alt="Logo-Icon" />
+            <h2 className='text-3xl sm:text-4xl font-bold '>Spering</h2>
           </div>
-          <div>
-            <img src={facebookIcon} alt="Facebook-Icon" />
-            <img src={twitterIcon} alt="Twitter-Icon" />
-            <img src={linkedinIcon} alt="LinkedIn-Icon" />
-            <img src={instagramIcon} alt="Instagram-Icon" />
-            <img src={youtubeIcon} alt="Youtube-Icon" />
+          <div className='flex justify-center items-center gap-x-1 sm:flex-row '>
+            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-7' src={facebookIcon} alt="Facebook-Icon" />
+            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-7' src={twitterIcon} alt="Twitter-Icon" />
+            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-7' src={linkedinIcon} alt="LinkedIn-Icon" />
+            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-7' src={instagramIcon} alt="Instagram-Icon" />
+            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-7' src={youtubeIcon} alt="Youtube-Icon" />
           </div>
         </div>
+        
       </section>
       <section>copy Write</section>
     </footer>
