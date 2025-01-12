24d10d46c299c18fb53a02e0cacf07ccd9d77df7
Basit Ali <opoali911@gmail.com>
2025-01-12 18:58:41 +0500

    final comit in category page yes only one left more

--- React_Project/SperingProject/src/Pages/Category/Category.jsx
+++ React_Project/SperingProject/src/Pages/Category/Category.jsx
@@ -11,34 +11,34 @@ const Category = () => {
     <>
       <section className="w-full  bg-[#1cbbb4] flex  justify-center">
         <div className="flex flex-col  items-center w-full sm:w-[90%] ">
-          <div className="mt-20 text-center">
+          <div className="mt-16 text-center">
             <h1 className="text-5xl font-semibold ">Category</h1>
-            <div className="w-52 h-1 bg-slate-200 mt-5"></div>
+            <div className="w-52 h-[1px] bg-slate-200 mt-5"></div>
           </div>
-          <div className="w-full sm:w-[80%] flex flex-wrap gap-28 justify-center sm:justify-between items-center mt-10 mb-20">
+          <div className="w-full sm:w-[80%] flex flex-wrap gap-28 justify-center sm:justify-between items-center mt-10 mb-32">
             <div className="flex flex-col justify-center items-center ">
               <img className="w-[50%]" src={categoryImage1} alt="" />
-              <h3 className="mt-3 text-2xl font-semibold">Design & Arts</h3>
+              <h3 className="mt-3 text-2xl ">Design & Arts</h3>
             </div>
             <div className="flex flex-col justify-center items-center ">
               <img className="w-[40%]" src={categoryImage2} alt="" />
-              <h3 className="mt-3 text-2xl font-semibold">Web Development</h3>
+              <h3 className="mt-3 text-2xl ">Web Development</h3>
             </div>
             <div className="flex flex-col justify-center items-center ">
               <img className="w-[50%]" src={categoryImage3} alt="" />
-              <h3 className="mt-3 text-2xl font-semibold">SEO Markting</h3>
+              <h3 className="mt-3 text-2xl ">SEO Markting</h3>
             </div>
             <div className="flex flex-col justify-center items-center ">
               <img className="w-[50%]" src={categoryImage4} alt="" />
-              <h3 className="mt-3 text-2xl font-semibold">Video Edting</h3>
+              <h3 className="mt-3 text-2xl ">Video Edting</h3>
             </div>
             <div className="flex flex-col justify-center items-center ">
-              <img className="w-[50%]" src={categoryImage5} alt="" />
-              <h3 className="mt-3 text-2xl font-semibold">Logo Design</h3>
+              <img className="w-[35%]" src={categoryImage5} alt="" />
+              <h3 className="mt-3 text-2xl ">Logo Design</h3>
             </div>
             <div className="flex flex-col justify-center items-center ">
               <img className="w-[50%]" src={categoryImage6} alt="" />
-              <h3 className="mt-3 text-2xl font-semibold">Game Design</h3>
+              <h3 className="mt-3 text-2xl">Game Design</h3>
             </div>
           </div>
         </div>
