23e9b598aa473119cf15f051f4417622995b18af
Basit Ali <opoali911@gmail.com>
2025-01-12 16:21:26 +0500

    the footer of the footer part is ready

--- React_Project/SperingProject/src/components/Fotter/Footer.jsx
+++ React_Project/SperingProject/src/components/Fotter/Footer.jsx
@@ -8,19 +8,19 @@ import {Link} from 'react-router-dom';
 const Footer = () => {
   return (
     <>
-    <footer className='w-full h-[50vh] flex flex-col items-center border-[3px] border-red-700 bg-[#1a2e35]'>
-      <section className='w-full sm:max-w-[1170px] bg-red-200 mt-10'>
-        <div className='flex flex-col sm:flex-row gap-3 justify-between items-center bg-red-300'>
-          <div className='flex justify-center items-center bg-red-700'>
-            <img className='w-10 sm:w-14' src={logoImage} alt="Logo-Icon" />
-            <h2 className='text-2xl sm:text-3xl font-bold '>Spering</h2>
+    <footer className='w-full h-[100vh] flex flex-col items-center border-[3px] border-red-700 bg-[#1a2e35]'>
+      <section className='w-full flex flex-col justify-center gap-20 sm:max-w-[90%] xl:max-w-[90%] bg-red-200 mt-10'>
+        <div className='flex flex-col sm:flex-row gap-3 justify-between items-center '>
+          <div className='flex justify-center items-center '>
+            <img className='w-10 sm:w-14 xl:w-20' src={logoImage} alt="Logo-Icon" />
+            <h2 className='text-2xl sm:text-3xl font-bold xl:text-6xl'>Spering</h2>
           </div>
-          <div className='flex justify-center items-center gap-x-1 sm:flex-row bg-blue-600'>
-            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-11' src={facebookIcon} alt="Facebook-Icon" />
-            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-11' src={twitterIcon} alt="Twitter-Icon" />
-            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-11' src={linkedinIcon} alt="LinkedIn-Icon" />
-            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-11' src={instagramIcon} alt="Instagram-Icon" />
-            <img className='mx-1 w-6 cursor-pointer sm:mx-4 sm:w-11' src={youtubeIcon} alt="Youtube-Icon" />
+          <div className='flex justify-center items-center gap-x-1 sm:flex-row '>
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={facebookIcon} alt="Facebook-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={twitterIcon} alt="Twitter-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={linkedinIcon} alt="LinkedIn-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={instagramIcon} alt="Instagram-Icon" />
+            <img className='mx-1 w-8 my-2 cursor-pointer sm:mx-4 xl:w-14 ' src={youtubeIcon} alt="Youtube-Icon" />
           </div>
         </div>
         <div className='grid grid-rows-4 text-center gap-3 sm:grid-cols-4'>
@@ -46,7 +46,9 @@ const Footer = () => {
           <div>NewsLetter</div>
         </div>
       </section>
-      <section>copy Write</section>
+      <section className='w-full text-center p-6 bg-white'>
+        <p className='text-md sm:text-2xl text-black  tracking-wider '>© 2025 All Rights Reserved By Free Html Templates</p>
+      </section>
     </footer>
       
     </>
