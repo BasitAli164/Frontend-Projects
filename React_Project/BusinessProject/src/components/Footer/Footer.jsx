69255902cf5a8a54e9dea2d60519c89d5b320820
Basit Ali <opoali911@gmail.com>
2025-01-13 20:34:03 +0500

    finall commit in the business app

--- React_Project/BusinessProject/src/components/Footer/Footer.jsx
+++ React_Project/BusinessProject/src/components/Footer/Footer.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import './Footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
-
-
+import React, { useEffect, useState } from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-        const arrowIcon = <FontAwesomeIcon icon={faArrowUp} />
-    
+  const arrowIcon = <FontAwesomeIcon icon={faArrowUp} />;
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollToTop=()=>{
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+
+  }
+  const listToScroll=()=>{
+    let height=250;
+    const windowScroll=document.body.scrollTop || document.documentElement.scrollTop;
+    (windowScroll>height)? setIsVisible(true):setIsVisible(false)
+  }
+  useEffect(()=>{
+    window.addEventListener("scroll",listToScroll)
+  })
   return (
     <>
-    <div className="container">
-        <footer className='footer'>
-            <p>@ All Right Reserved 2023 Design By <a href="/">Abdupa911</a></p>
+      <div className="container">
+        <footer className="footer">
+          <p>
+            @ All Right Reserved 2023 Design By <a href="/">Abdupa911</a>
+          </p>
         </footer>
-    </div>
-    <div className="scroll_top">
-        {arrowIcon}
-    </div>
-
-      
+      </div>
+      {isVisible &&(
+         <div className="scroll_top" onClick={scrollToTop}>{arrowIcon}</div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
