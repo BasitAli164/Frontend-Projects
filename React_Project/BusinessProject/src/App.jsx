073cff614e43b6781c35e2712c1d76429f58c1b0
Basit Ali <opoali911@gmail.com>
2025-01-13 19:02:54 +0500

    now start the contact section of the business app

--- React_Project/BusinessProject/src/App.jsx
+++ React_Project/BusinessProject/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Service from './components/Service/Service'
 import Blog from './components/Blog/Blog'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
      <About/>
      <Service/>
      <Blog/>
+     <Contact/>
     </>
   )
 }
