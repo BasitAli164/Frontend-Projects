847fe2a9a90545e55572f8f4c417575e44a22500
Basit Ali <opoali911@gmail.com>
2025-01-13 20:21:26 +0500

    now working svg icon

--- React_Project/BusinessProject/src/components/Footer/Footer.jsx
+++ React_Project/BusinessProject/src/components/Footer/Footer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const Footer = () => {
+        const arrowIcon = <FontAwesomeIcon icon={faArrowUp} />
+    
   return (
     <>
     <div className="container">
         <footer className='footer'>
             <p>@ All Right Reserved 2023 Design By <a href="/">Abdupa911</a></p>
         </footer>
+    </div>
+    <div className="scroll_top">
+        {arrowIcon}
     </div>
 
       
