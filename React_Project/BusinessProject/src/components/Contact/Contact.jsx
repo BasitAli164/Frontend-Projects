61bd3891eead37c5bc3bffabde10f539b612cf2c
Basit Ali <opoali911@gmail.com>
2025-01-13 19:05:22 +0500

    almost ready the structure of the contact section

--- React_Project/BusinessProject/src/components/Contact/Contact.jsx
+++ React_Project/BusinessProject/src/components/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Contact.css'
+import contactImage from '../../assets/contact/contact.jpg'
 
 const Contact = () => {
   return (
     <>
-    <section id="contact"></section>
+    <section id="contact">
+        <div className="container">
+            <div className="contact_wrapper">
+                <div className="contact_col">
+                    <div className="contact_img">
+                        <img src={contactImage} alt="Contact" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
       
     </>
   )
