773bebff5e10396b1bb99efb84ad52d54d08baf7
Basit Ali <opoali911@gmail.com>
2025-01-13 19:13:58 +0500

    make or build the basic structure of the contact section

--- React_Project/BusinessProject/src/components/Contact/Contact.jsx
+++ React_Project/BusinessProject/src/components/Contact/Contact.jsx
@@ -13,6 +13,28 @@ const Contact = () => {
                         <img src={contactImage} alt="Contact" />
                     </div>
                 </div>
+                <div className="contact_col">
+                    <h2>Contact Us</h2>
+                    <form >
+                        <div className="input_wrapper">
+                            <input type="text" name="" id=""    className='form-control' placeholder='Your Name...' autoComplete='off' />
+                        </div>
+                        <div className="input_wrapper">
+                            <input type="text" name="" id=""    className='form-control' placeholder='Your Phone...' autoComplete='off' />
+                        </div>
+                        <div className="input_wrapper">
+                            <input type="email" name="" id=""    className='form-control' placeholder='Your Email...' autoComplete='off' />
+                        </div>
+                        <div className="input_wrapper">
+                            <textarea name="" id="" placeholder='Write a Message...'></textarea>
+                        </div>
+                        <div className="btn_wrapper">
+                            <button type="submit" className='btn'>
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
