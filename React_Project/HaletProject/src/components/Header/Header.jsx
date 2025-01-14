6c265eb2a0e2b6a39b99fb78ec02d156925f8e8b
Basit Ali <opoali911@gmail.com>
2025-01-14 14:46:45 +0500

    fixed the bugs of the warning of tailwind

--- React_Project/HaletProject/src/components/Header/Header.jsx
+++ React_Project/HaletProject/src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <>
-    <section>From header</section>
+    <section >
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/shop'>Shop</Link></li>
+        <li><Link to='/blog'>Blog</Link></li>
+
+      </ul>
+    </section>
       
     </>
   )
