5c6094fb3946eeaf03099c521e2269694ba1042f
Basit Ali <opoali911@gmail.com>
2025-01-14 18:41:23 +0500

    the button component is almost ready

--- React_Project/HaletProject/src/components/Button/Button.jsx
+++ React_Project/HaletProject/src/components/Button/Button.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ btnText, borderRadius }) => {
   return (
     <>
-    <h1>Button</h1>
-      
+      <button className="px-14 py-[13px] text-[#fd9c6b] border-[1px] border-dotted border-[#fd9c6b] hover:text-white hover:bg-orange-400 duration-[.5s]" style={{borderRadius:`${borderRadius}`}}>
+        {btnText}
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
