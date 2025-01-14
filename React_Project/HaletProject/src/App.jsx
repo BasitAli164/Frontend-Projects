30f892317f4e97919fb8b5c555dc97cccb2f7976
Basit Ali <opoali911@gmail.com>
2025-01-14 13:34:52 +0500

    installing the tailwind or integrate the tailwind css framework

--- React_Project/HaletProject/src/App.jsx
+++ React_Project/HaletProject/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 
 const App = () => {
   return (
     <div className='h-[123vh] w-full p-90 flex justify-center items-center bg-slate-500'>
-      <h1 className='text-red-500'>Halet Project</h1>
+      <h1 className='text-red-500'> Project</h1>
     </div>
   )
 }
