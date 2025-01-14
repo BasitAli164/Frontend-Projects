edc4f4ec3637124931c43fcc8d1e00d1ea3172a7
Basit Ali <opoali911@gmail.com>
2025-01-14 19:10:34 +0500

    almost basic components are ready

--- React_Project/HaletProject/src/Pages/Home/Home.jsx
+++ React_Project/HaletProject/src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import Button from '../../components/Button/Button'
 const Home = () => {
   return (
     <>
-    <section  className='w-full h-screen bg-red-300 bg-center bg-cover z-100' style={{backgroundImage:`url(${bgImage})`}}>
+    <section  className='w-full h-screen bg-red-300 bg-center bg-cover z-100 ' style={{backgroundImage:`url(${bgImage})`}}>
     <h1>From Home</h1>
     <Button btnText="Shop Now" borderRadius='5px'/>
 
--- React_Project/HaletProject/src/components/Button/Button.jsx
+++ React_Project/HaletProject/src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({ btnText, borderRadius }) => {
+const Button = ({ btnText, borderRadius,bgColor,textColo }) => {
   return (
     <>
-      <button className="px-14 py-[13px] text-[#fd9c6b] border-[1px] border-dotted border-[#fd9c6b] hover:text-white hover:bg-orange-400 duration-[.5s]" style={{borderRadius:`${borderRadius}`}}>
+      <button className="px-14 py-[13px] text-[#fd9c6b] bg-transparent text-lg border-[1px] border-dotted border-[#fd9c6b] hover:text-white hover:bg-[#fd9c6b] duration-[.5s]" style={{borderRadius:`${borderRadius}`}}>
         {btnText}
       </button>
     </>
