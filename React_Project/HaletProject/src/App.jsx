9a2d14378d19819fc9cf744865a461198b6d828b
Basit Ali <opoali911@gmail.com>
2025-01-14 13:24:54 +0500

    start the halet project

--- React_Project/HaletProject/src/App.jsx
+++ React_Project/HaletProject/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Halet Project</h1>
+    <div className='h-[123vh] w-full p-90 flex justify-center items-center bg-slate-500'>
+      <h1 className='text-red-500'>Halet Project</h1>
     </div>
   )
 }
