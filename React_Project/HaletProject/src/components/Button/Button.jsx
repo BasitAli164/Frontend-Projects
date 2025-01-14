4971d787f713821d433c2361e1b9b566c1583b96
Basit Ali <opoali911@gmail.com>
2025-01-14 23:08:50 +0500

    the responsiveness part of the home page is also complete

--- React_Project/HaletProject/src/components/Button/Button.jsx
+++ React_Project/HaletProject/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ btnText, borderRadius }) => {
   return (
     <>
       <button
-        className="px-14 py-[10px] text-[#fd9c6b] bg-transparent text-lg border-[1px] border-dotted border-[#fd9c6b] hover:text-white hover:bg-[#fd9c6b] duration-[.5s]"
+        className="px-10 py-[5px] sm:px-14  sm:py-[10px] text-[#fd9c6b] bg-transparent text-lg border-[1px] border-solid border-[#fff] hover:text-white hover:bg-[#fd9c6b] duration-[.5s]"
         style={{ borderRadius: `${borderRadius}` }}
       >
         {btnText}
