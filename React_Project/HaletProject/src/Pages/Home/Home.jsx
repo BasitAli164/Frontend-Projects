e35afaf1b24e94cad397adc74ef29558e9fb9de2
Basit Ali <opoali911@gmail.com>
2025-01-14 22:55:04 +0500

    the home page almost ready

--- React_Project/HaletProject/src/Pages/Home/Home.jsx
+++ React_Project/HaletProject/src/Pages/Home/Home.jsx
@@ -6,9 +6,19 @@ const Home = () => {
   return (
     <>
       <section
-        className="w-full h-[150vh] bg-red-300 bg-center bg-cover   z-100 "
+        className="w-full h-[150vh] bg-red-300 bg-center bg?>   z-100 "
         style={{ backgroundImage: `url(${bgImage})` }}
       >
+        <div className="w-[60%] sm:w-[40%] h-[100%] sm:h-[50%] flex justify-center  ml-10 flex-col gap-y-20"> 
+          <div className="mt-0 sm:mt-40">
+            <h1 className="text-white text-3xl sm:text-5xl font-bold">Best Jewellery Collection</h1>
+         
+            <p className=" text-sm sm:text-lg text-white text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
+          </div>
+          <div>
+            <Button btnText="Shop Now"/>
+          </div>
+        </div>
         
       </section>
     </>
--- React_Project/HaletProject/src/components/Button/Button.jsx
+++ React_Project/HaletProject/src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ btnText, borderRadius }) => {
   return (
     <>
       <button
-        className="px-14 py-[13px] text-[#fd9c6b] bg-transparent text-lg border-[1px] border-dotted border-[#fd9c6b] hover:text-white hover:bg-[#fd9c6b] duration-[.5s]"
+        className="px-14 py-[10px] text-[#fd9c6b] bg-transparent text-lg border-[1px] border-dotted border-[#fd9c6b] hover:text-white hover:bg-[#fd9c6b] duration-[.5s]"
         style={{ borderRadius: `${borderRadius}` }}
       >
         {btnText}
