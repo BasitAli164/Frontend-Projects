5bc4239d350b5725d9cb6cd45f0d5cf429458e02
Basit Ali <opoali911@gmail.com>
2025-01-14 13:09:00 +0500

    know start the helate project

--- React_Project/Food_Delivery_App/src/App.jsx
+++ React_Project/Food_Delivery_App/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 const App = () => {
   return (
     <>
-    <h1>Food Delivery App </h1>
+    <h1 className='bg-gradient-to-r from-amber-800 to-amber-400 bg-clip-text text-transparent'>Food Delivery App </h1>
       
     </>
   )
