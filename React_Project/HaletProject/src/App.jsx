71585b1f96d4c7b9f02bae25d0cb63828d7c1a84
Basit Ali <opoali911@gmail.com>
2025-01-14 14:02:14 +0500

    now integrate the tailwind with this project

--- React_Project/HaletProject/src/App.jsx
+++ React_Project/HaletProject/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className='h-[123vh] w-full p-90 flex justify-center items-center bg-slate-500'>
+    <div className='h-[223vh] w-full  flex justify-center items-center bg-slate-500'>
       <h1 className='text-red-500'> Project</h1>
     </div>
   )
