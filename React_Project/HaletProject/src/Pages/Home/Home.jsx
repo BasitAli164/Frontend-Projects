6fb08babfd8b0a17799fb135b0a8622a95fa686d
Basit Ali <opoali911@gmail.com>
2025-01-14 18:29:18 +0500

    now start the button component

--- React_Project/HaletProject/src/Pages/Home/Home.jsx
+++ React_Project/HaletProject/src/Pages/Home/Home.jsx
@@ -4,11 +4,11 @@ import bgImage from '../../assets/images/slider-bg.jpg'
 const Home = () => {
   return (
     <>
-    <div className='w-full h-screen bg-red-300 bg-center bg-cover z-100' style={{backgroundImage:`url(${bgImage})`}}>
+    <section  className='w-full h-screen bg-red-300 bg-center bg-cover z-100' style={{backgroundImage:`url(${bgImage})`}}>
     <h1>From Home</h1>
 
       
-      </div>      
+      </section>      
     </>
   )
 }
