470b5119a4667a3b3002e55b6815734d631f874f
Basit Ali <opoali911@gmail.com>
2025-01-15 18:38:31 +0500

    formate document with prettier

--- React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
+++ React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
@@ -112,51 +112,78 @@ const AcmeRockets = () => {
               Our Testimonials
             </h2>
             <figure className="my-12 ">
-                <blockquote className="bg-teal-400 border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative">
-                <p className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
+              <blockquote className="bg-teal-400 border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative">
+                <p
+                  className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
                 before:translate-y-2 after:content-['\201d'] after:font-serif after:absolute after:bottom-[-50px]  after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-x-2
-                after:translate-y-2">
-                   Acme has always been there for me. Their Explorer rocket arrived in a wooden crate as expected. Lifelong customer! A++ shopping experience!
+                after:translate-y-2"
+                >
+                  Acme has always been there for me. Their Explorer rocket
+                  arrived in a wooden crate as expected. Lifelong customer! A++
+                  shopping experience!
                 </p>
-                </blockquote>
-                <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
-                    &#8212;Wile E. Coyete, Genius 
-                </figcaption>
+              </blockquote>
+              <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
+                &#8212;Wile E. Coyete, Genius
+              </figcaption>
             </figure>
             <figure className="my-12 ">
-                <blockquote className="bg-teal-400 border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative">
-                <p className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
+              <blockquote className="bg-teal-400 border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative">
+                <p
+                  className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
                 before:translate-y-2 after:content-['\201d'] after:font-serif after:absolute after:bottom-[-50px]  after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-x-2
-                after:translate-y-2">
-                   The Acme Adventurer Rocket has thwarted my Illudium Q-36 Explosive Space Modulator on several occassions. This makes me very, very angry! Nevertheless, K-9 and I have awarded Acme the Martian contract for space exploration rockets based on Acme's quality and sturdy designs.
+                after:translate-y-2"
+                >
+                  The Acme Adventurer Rocket has thwarted my Illudium Q-36
+                  Explosive Space Modulator on several occassions. This makes me
+                  very, very angry! Nevertheless, K-9 and I have awarded Acme
+                  the Martian contract for space exploration rockets based on
+                  Acme's quality and sturdy designs.
                 </p>
-                </blockquote>
-                <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
-                    &#8212;Marvin the Martian & K-9
-                </figcaption>
+              </blockquote>
+              <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
+                &#8212;Marvin the Martian & K-9
+              </figcaption>
             </figure>
             <figure className="my-12 ">
-                <blockquote className="border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative bg-teal-400 ">
-                <p className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
+              <blockquote className="border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative bg-teal-400 ">
+                <p
+                  className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
                 before:translate-y-2 after:content-['\201d'] after:font-serif after:absolute after:bottom-[-50px]  after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-x-2
-                after:translate-y-2">
-                   I knew I not only wanted — <span className="italic">I needed </span> — Acme's Infinity Rocket for my mission in space. Acme delievered in one day! Nothing says Take me to your leader like Acme's Infinity Rocket! 💯
+                after:translate-y-2"
+                >
+                  I knew I not only wanted —{" "}
+                  <span className="italic">I needed </span> — Acme's Infinity
+                  Rocket for my mission in space. Acme delievered in one day!
+                  Nothing says Take me to your leader like Acme's Infinity
+                  Rocket! 💯
                 </p>
-                </blockquote>
-                <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
-                    &#8212;Buzz Lightyear
-                </figcaption>
+              </blockquote>
+              <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
+                &#8212;Buzz Lightyear
+              </figcaption>
             </figure>
           </section>
           <hr className="mx-auto bg-white dark:bg-white w-1/2 h-[2px] " />
           <section id="contact" className="p-6 my-12 scroll-mt-16">
-
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-600 dark:text-white">
               Contact Us
             </h2>
-            <form action="" className="max-w-4xl mx-auto text-2xl sm:text-3xl flex flex-col items-left gap-4">
-                <label htmlFor="subject">Subject</label>
-                <input type="text" id="subject" name="subject" required minLength={3} maxLength={60} placeholder="Your Subject" className="w-full text-black text-2xl  sm:text-3xl p-3  rounded-xl border border-solid border-slate-900 dark:border-none" />
+            <form
+              action=""
+              className="max-w-4xl mx-auto text-2xl sm:text-3xl flex flex-col items-left gap-4"
+            >
+              <label htmlFor="subject">Subject</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                required
+                minLength={3}
+                maxLength={60}
+                placeholder="Your Subject"
+                className="w-full text-black text-2xl  sm:text-3xl p-3  rounded-xl border border-solid border-slate-900 dark:border-none"
+              />
             </form>
           </section>
         </main>
