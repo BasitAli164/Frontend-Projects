21afbd4c69e96428bc8a63b48834a45aa9ac96d6
Basit Ali <opoali911@gmail.com>
2025-01-15 18:10:19 +0500

    fixed some more bugs

--- React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
+++ React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
@@ -114,9 +114,9 @@ const AcmeRockets = () => {
             <figure className="my-12 ">
                 <blockquote className="border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative">
                 <p className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
-                before:translate-y-2 after:content-['\201d'] after:font-serif after:absolute after:bottom-0  after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-x-2
+                before:translate-y-2 after:content-['\201d'] after:font-serif after:absolute after:bottom-[-60px]  after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-x-2
                 after:translate-y-2">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ex rerum earum, alias deserunt veniam tempora sapiente dolor culpa, eaque aut eum non!
+                   Acme has been always there for me. Their Explorer rocket arrived in a wooden  crate as expected. Life-long customer! A++ Shopping experience.
                 </p>
                 <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
                     &#8212;Wile E. Coyete, Genius 
