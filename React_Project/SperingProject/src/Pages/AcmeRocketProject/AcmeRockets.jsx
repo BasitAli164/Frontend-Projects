75be1d657a17521383b959f1fb75048ba1339e34
Basit Ali <opoali911@gmail.com>
2025-01-15 18:30:46 +0500

    start the contact section of the acme rocket project

--- React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
+++ React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
@@ -149,9 +149,10 @@ const AcmeRockets = () => {
             </figure>
           </section>
           <hr className="mx-auto bg-white dark:bg-white w-1/2 h-[2px] " />
-          <section id="contact" className="p-6 my-12">
+          <section id="contact" className="p-6 my-12 scroll-mt-16">
+
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-600 dark:text-white">
-              Contact Us{" "}
+              Contact Us
             </h2>
           </section>
         </main>
