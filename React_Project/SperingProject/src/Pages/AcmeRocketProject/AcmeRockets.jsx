e1c8b4b417b9f96b1bdc8cee96c46025126247e4
Basit Ali <opoali911@gmail.com>
2025-01-15 18:46:17 +0500

    the contact section is complete

--- React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
+++ React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
@@ -186,6 +186,7 @@ const AcmeRockets = () => {
               />
               <label htmlFor="message">Message:</label>
               <textarea name="message" id="message" cols={30} rows={10} placeholder="Your Message" className="w-full text-black text-2xl  sm:text-3xl p-3  rounded-xl border border-solid border-slate-900 dark:border-none"></textarea>
+              <button className="bg-teal-700 hover:bg-teal-600 active:bg-teal-500 text-white p-3 w-48 rounded-xl border border-solid border-slate-900 dark:border-none">Submit</button>
             </form>
           </section>
         </main>
