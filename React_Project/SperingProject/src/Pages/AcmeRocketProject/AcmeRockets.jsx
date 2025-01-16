5bad4b5890b69986512f0654f9f3d54b3a928b84
Basit Ali <opoali911@gmail.com>
2025-01-16 18:02:54 +0500

    write first quote

--- React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
+++ React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
@@ -111,6 +111,18 @@ const AcmeRockets = () => {
             <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-600 dark:text-white">
               Our Testimonials
             </h2>
+            <figure className="my-12 ">
+                <blockquote className="border-teal-600 dark:bg-black pl-14 py-12 rounded-3xl relative">
+                <p className="text-2xl sm-sm:text-3xl text-left mt-2 text-white dark:text-slate-400 before:content-['\201c'] before:font-serif before:absolute before:top-0 before:left-0 before:text-9xl before:text-white before:opacity-25 before:transform before:translate-x-2
+                before:translate-y-2 after:content-['\201d'] after:font-serif after:absolute after:bottom-0  after:right-0 after:text-9xl after:text-white after:opacity-25 after:transform after:translate-x-2
+                after:translate-y-2">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates ex rerum earum, alias deserunt veniam tempora sapiente dolor culpa, eaque aut eum non!
+                </p>
+                <figcaption className="italic text-xl sm:text-2xl text-right text-slate-500 dark:text-slate-400 ">
+                    &#8212;Wile E. Coyete, Genius 
+                </figcaption>
+                </blockquote>
+            </figure>
           </section>
           <hr className="mx-auto bg-white dark:bg-white w-1/2 h-[2px] " />
           <section id="contact" className="p-6 my-12">
