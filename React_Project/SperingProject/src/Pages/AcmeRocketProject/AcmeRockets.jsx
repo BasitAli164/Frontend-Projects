6748bcca9fe77091b60ee2dea6bbfea5227bddaa
Basit Ali <opoali911@gmail.com>
2025-01-16 16:07:30 +0500

    add the smoth ness in the scroll bar

--- React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
+++ React_Project/SperingProject/src/Pages/AcmeRocketProject/AcmeRockets.jsx
@@ -28,7 +28,7 @@ const AcmeRockets = () => {
 
         </header>
         <main className='max-w-4xl mx-auto'>
-            <section id='hero' className='flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12'>
+            <section id='hero' className='flex flex-col-reverse justify-center sm:flex-row p-6 items-center gap-8 mb-12 scroll-mt-40'>
                 <article className='sm:w-1/2'>
                 <h2 className='max-w-md text-3xl font-bold text-center sm:text-4xl sm:text-left text-slate-900 dark:text-white'>We Boldy Go <span className="text-indigo-700 dark:text-indigo-300"> Where No Rocket</span> Has Gone Before ...</h2>
                 <p className="max-w-md text-2xl mt-4 text-center sm:text-left text-slate-700 dark:text-slate-400">
