9343162a44a1e821b1af80a2c041a1b8aa6e4761
Basit Ali <opoali911@gmail.com>
2025-01-17 18:43:53 +0500

    working on the footer component ,almost complete this component

--- React_Project/HaletProject/src/components/Footer/Footer.jsx
+++ React_Project/HaletProject/src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import facebookIcon from '../../assets/images/fb.png';
-import twitterIcon from '../../assets/images/twitter.png';
-import linkedinIcon from '../../assets/images/linkedin.png';
-import footerImage1 from '../../assets/images/p1.png';
-import footerImage2 from '../../assets/images/p2.png';
+import facebookIcon from "../../assets/images/fb.png";
+import twitterIcon from "../../assets/images/twitter.png";
+import linkedinIcon from "../../assets/images/linkedin.png";
+import footerImage1 from "../../assets/images/p1.png";
+import footerImage2 from "../../assets/images/p2.png";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
@@ -23,55 +23,100 @@ const Footer = () => {
             </div>
             <div className="flex justify-center items-center gap-x-5">
               <div className="w-10 h-10 border-[1px] border-solid border-white  flex justify-center items-center hover:border-[#fd9c6b] cursor-pointer">
-              <img src={facebookIcon} alt="Facebook-Icon" className="w-5 hover:text-[#fd9c6b]" />
-
+                <img
+                  src={facebookIcon}
+                  alt="Facebook-Icon"
+                  className="w-5 hover:text-[#fd9c6b]"
+                />
               </div>
               <div className="w-10 h-10 border-[1px] border-solid border-whiter flex justify-center items-center hover:border-[#fd9c6b] cursor-pointer">
-              <img src={twitterIcon} alt="Twitter-Icon" className="w-5 hover:text-[#fd9c6b]" />
-
+                <img
+                  src={twitterIcon}
+                  alt="Twitter-Icon"
+                  className="w-5 hover:text-[#fd9c6b]"
+                />
               </div>
               <div className="w-10 h-10 border-[1px] border-solid border-white  flex justify-center items-center hover:border-[#fd9c6b] cursor-pointer">
-              <img src={linkedinIcon} alt="Linkedin-Icon" className="w-5 hover:text-[#fd9c6b]" />
-
+                <img
+                  src={linkedinIcon}
+                  alt="Linkedin-Icon"
+                  className="w-5 hover:text-[#fd9c6b]"
+                />
               </div>
             </div>
-            
           </div>
-          <div className="flex justify-between flex-col sm:flex-row items-center flex-wrap">
-            <div className="flex flex-col gap-y-3">
+          <div className="grid grid-rows-[auto,auto,auto,auto] sm:grid-cols-4 gap-10">
+            <div className="flex flex-col gap-y-3 items-center">
               <h2 className="text-white text-3xl">Menu</h2>
               <ul className="flex flex-col gap-y-2">
-                <Link className="text-white text-xl hover:text-[#fd9c6b]" to="/">Home</Link>
-                <Link className="text-white text-xl hover:text-[#fd9c6b]" to="/about">About</Link>
-                <Link className="text-white text-xl hover:text-[#fd9c6b]" to="/shop">Shop</Link>
-                <Link className="text-white text-xl hover:text-[#fd9c6b]" to="/blog">Blog</Link>
+                <Link
+                  className="text-white text-xl hover:text-[#fd9c6b]"
+                  to="/"
+                >
+                  Home
+                </Link>
+                <Link
+                  className="text-white text-xl hover:text-[#fd9c6b]"
+                  to="/about"
+                >
+                  About
+                </Link>
+                <Link
+                  className="text-white text-xl hover:text-[#fd9c6b]"
+                  to="/shop"
+                >
+                  Shop
+                </Link>
+                <Link
+                  className="text-white text-xl hover:text-[#fd9c6b]"
+                  to="/blog"
+                >
+                  Blog
+                </Link>
               </ul>
             </div>
             <div className="flex flex-col justify-center gap-y-6">
-            <h2 className="text-white text-3xl text-center sm:text-left">Instagram</h2>
-            <div className="flex flex-col justify-center items-center gap-y-4 ">
-              <div className="flex flex-col sm:flex-row gap-y-3 sm:gap-y-0 justify-center items-center gap-x-4 ">
-                <div className="w-12 h-12 bg-white flex justify-center items-center">
-                <img className="w-5 " src={footerImage1} alt="gooter-Image1" />
-
+              <h2 className="text-white text-3xl text-center sm:text-left">
+                Instagram
+              </h2>
+              <div className="flex flex-col justify-center items-center gap-y-4 ">
+                <div className="flex flex-col sm:flex-row gap-y-3 sm:gap-y-0 justify-center items-center gap-x-4 ">
+                  <div className="w-12 h-12 bg-white flex justify-center items-center">
+                    <img
+                      className="w-5 "
+                      src={footerImage1}
+                      alt="gooter-Image1"
+                    />
+                  </div>
+                  <p className="text-white ">
+                    long established fact that a reader
+                  </p>
                 </div>
-                <p className="text-white ">long established fact that a reader</p>
-              </div>
-              <div className="flex flex-col gap-y-3 sm:flex-row justify-center items-center gap-x-5">
-                <div className="w-12 h-12 bg-white flex justify-center items-center">
-                <img className="w-5 " src={footerImage2} alt="gooter-Image1" />
-
+                <div className="flex flex-col gap-y-3 sm:flex-row justify-center items-center gap-x-5">
+                  <div className="w-12 h-12 bg-white flex justify-center items-center">
+                    <img
+                      className="w-5 "
+                      src={footerImage2}
+                      alt="gooter-Image1"
+                    />
+                  </div>
+                  <p className="text-white ">
+                    long established fact that a reader
+                  </p>
                 </div>
-                <p className="text-white ">long established fact that a reader</p>
               </div>
-              
-
             </div>
-
+            <div>
+            <h2 className="text-white text-3xl text-center sm:text-left">
+                About Us 
+              </h2>
+              <div>
+                <p className="text-white text-lg">when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to</p>
+              </div>
             </div>
-            <div>About Us</div>
             <div>Contact Us</div>
           </div>
+          
         </section>
         <section className="w-full p-6 bg-[#e9e9e9]">
           <p className="text-black text-xl  text-center">
