fdb16751c7eae8bcb4efe60149450b0464caebef
Basit Ali <opoali911@gmail.com>
2025-01-17 14:53:07 +0500

    almost the header is complete

--- React_Project/HaletProject/src/components/Header/Header.jsx
+++ React_Project/HaletProject/src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 const Header = () => {
+  const [toggle,setToggle]=useState(false);
+  const handleToggle=()=>{
+    console.log("Basit is")
+    setToggle(!toggle)
+  }
+
   return (
     <>
-    <header className='w-full  bg-[#c69c72] '>
+    <header className='w-full  bg-[#c69c72]  sticky top-0 z-10'>
       <section className='flex justify-between sm:justify-around items-center  h-20'>
         <div>
           <h1 className=' text-2xl sm:text-3xl text-white font-semibold uppercase'>Healet</h1>
@@ -15,12 +21,23 @@ const Header = () => {
         <Link className='text-lg sm:text-xl font-semibold text-white uppercase' to='/shop'>Shop</Link>
         <Link className='text-lg sm:text-xl font-semibold text-white uppercase' to='/blog'>Blog</Link>
       </nav>
-      <div className='sm:hidden text-3xl text-white'>
+      <button onClick={handleToggle} className='sm:hidden text-3xl text-white border-none outline-none'>
         &#9776;
-      </div>
+      </button>
       </section>
-      <section>
-        
+      <section className={`absolute top-0 z-50 bg-black w-full flex-col justify-center origin-top animate-open-menu ${toggle?'flex':'hidden'} `}>
+      <button
+       className='text-5xl self-end px-6 text-white'
+       onClick={handleToggle}
+      >
+        &times;
+      </button>
+      <nav className='flex flex-col gap-y-10 items-center h-screen justify-center'>
+        <Link onClick={handleToggle} className='text-lg sm:text-xl font-semibold text-white uppercase' to='/'>Home</Link>
+        <Link onClick={handleToggle} className='text-lg sm:text-xl font-semibold text-white uppercase' to='/about'>About</Link>
+        <Link onClick={handleToggle} className='text-lg sm:text-xl font-semibold text-white uppercase' to='/shop'>Shop</Link>
+        <Link onClick={handleToggle} className='text-lg sm:text-xl font-semibold text-white uppercase' to='/blog'>Blog</Link>
+      </nav>
       </section>
     </header>
       
