a540753d3c7f8548081418f81b5e0a68e50a254a
Basit Ali <opoali911@gmail.com>
2025-01-17 15:58:29 +0500

    working in footer

--- React_Project/HaletProject/src/components/Footer/Footer.jsx
+++ React_Project/HaletProject/src/components/Footer/Footer.jsx
@@ -3,15 +3,23 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <footer className='w-full h-[65vh] bg-[#000] opacity-90'>
-        <section>
-          <div>
-            <input type="text" />
-            <span className='text-white'>&#8594;</span>
+      <footer className='w-full  bg-[#160e0b]'>
+        <section className='flex flex-col justify-center p-10'>
+          <div className='ml-10 relative flex'>
+            <input className='w-[60%] h-10 ' type="text" />
+            <span className='text-white block w-3 h-3 bg-[#]'>&#8594;</span>
           </div>
-          
+          <div>
+          <div>menu</div>
+          <div>Instagram</div>
+          <div>About Us</div>
+          <div>About Us</div>
+          <div>Contact Us</div>
+          </div>
         </section>
-        <section></section>
+        <section className='w-full p-6 bg-[#e9e9e9]'>
+          <p className='text-black text-xl  text-center'>&copy; All Rights Reserved By Free Html Temlate</p>
+        </section>
       </footer>
     </>
   )
