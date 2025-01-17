b58b6243b703f2acd1143eb61bd72dc8257dec90
Basit Ali <opoali911@gmail.com>
2025-01-17 23:05:06 +0500

    about is complete

--- React_Project/HaletProject/src/Pages/About/About.jsx
+++ React_Project/HaletProject/src/Pages/About/About.jsx
@@ -7,15 +7,15 @@ const About = () => {
     <>
   <section className='h-screen w-full grid grid-rows-[auto, auto] md:grid-cols-2 relative' >
     <div 
-    className='justify-self-center self-center xl:after:w-[250px] xl:after:h-[250px] xl:after:bg-black xl:after:absolute xl:after:top-20 xl:after:left-10  lg:after:w-[250px] lg:after:h-[250px] lg:after:bg-black lg:after:absolute lg:after:top-32 lg:after:left-2  md:after:w-[200px] md:after:h-[200px] md:after:bg-black md:after:absolute md:after:top-40 md:after:left-2 -z-10]'
+    className='justify-self-center self-center xl:after:w-[250px] xl:after:h-[250px] xl:after:bg-black xl:after:absolute xl:after:top-20 xl:after:left-10  lg:after:w-[250px] lg:after:h-[250px] lg:after:bg-black lg:after:absolute lg:after:top-32 lg:after:left-2  md:after:w-[200px] md:after:h-[200px] md:after:bg-black md:after:absolute md:after:top-24 md:after:left-1 -z-10]'
     >
-      <img className='sm:w-[350px] md:w-[300px] lg:w-[440px] xl:w-[500px] relative z-[2] ' src={aboutImage} alt="About-Image" />
+      <img className='w-[250px] sm:w-[350px] md:w-[350px] lg:w-[440px] xl:w-[500px] 2xl:w-[700px] relative z-[2] ' src={aboutImage} alt="About-Image" />
     </div>
-    <div className=' justify-self-center self-center '>
-       <div  >
-       <h2 className='text-[32px] font-bold my-3 font-serif'>About us</h2>
-       <p className='text-[16px] text-justify font-serif w-[80%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem eum consequuntur ipsam repellat dolor soluta aliquid laborum, eius odit consectetur vel quasi in quidem, eveniet ab est corporis tempore.</p>
-       <div className='mt-10'>
+    <div className='place-items-center place-self-center'>
+       <div  className='flex flex-col justify-start items-center'>
+       <h2 className='text-[24px] sm:text-[32px] font-bold my-3 font-serif text-left'>About us</h2>
+       <p className='text-[14px] sm:text-[16px] text-justify font-serif w-[80%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorem eum consequuntur ipsam repellat dolor soluta aliquid laborum, eius odit consectetur vel quasi in quidem, eveniet ab est corporis tempore.</p>
+       <div className='mt-2 sm:mt-10'>
         <Button btnText="Read More"/>
        </div>
        </div>
