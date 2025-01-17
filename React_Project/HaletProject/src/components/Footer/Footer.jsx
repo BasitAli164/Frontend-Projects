b1efb739dc6fb791f7966862b7066080c3ba8c86
Basit Ali <opoali911@gmail.com>
2025-01-17 15:47:58 +0500

    working on the footer

--- React_Project/HaletProject/src/components/Footer/Footer.jsx
+++ React_Project/HaletProject/src/components/Footer/Footer.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      
+      <footer className='w-full h-[65vh] bg-[#000] opacity-90'>
+        <section>
+          <div>
+            <input type="text" />
+            <span className='text-white'>&#8594;</span>
+          </div>
+          
+        </section>
+        <section></section>
+      </footer>
     </>
   )
 }
