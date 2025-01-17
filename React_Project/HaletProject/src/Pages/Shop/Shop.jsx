893b2d2f45aab6c131416d5db0e1a39631f9f16a
Basit Ali <opoali911@gmail.com>
2025-01-18 00:10:09 +0500

    the shop page is finished

--- React_Project/HaletProject/src/Pages/Shop/Shop.jsx
+++ React_Project/HaletProject/src/Pages/Shop/Shop.jsx
@@ -13,59 +13,59 @@ const Shop = () => {
   const productDetail = [
     {
       img: product1,
-      alt:"Nacklace-Image",
+      alt: "Nacklace-Image",
       prouductName: "Nacklace",
       price: "$200",
-      proudctStatus:'Old'
+      proudctStatus: "Old",
     },
     {
       img: product2,
-      alt:"Nacklace-Image",
+      alt: "Nacklace-Image",
       prouductName: "Nacklace",
       price: "$300",
-      proudctStatus:'New'
+      proudctStatus: "New",
     },
     {
       img: product3,
-      alt:"Nacklace-Image",
+      alt: "Nacklace-Image",
       prouductName: "Nacklace",
       price: "$110",
-      proudctStatus:'Old'
+      proudctStatus: "Old",
     },
     {
       img: product4,
-      alt:"Ring-Image",
+      alt: "Ring-Image",
       prouductName: "Ring",
       price: "$45",
-      proudctStatus:'New'
+      proudctStatus: "New",
     },
     {
       img: product5,
-      alt:"Ring-Image",
+      alt: "Ring-Image",
       prouductName: "Ring",
       price: "$95",
-      proudctStatus:'Old'
+      proudctStatus: "Old",
     },
     {
       img: product6,
-      alt:"Earrings-Image",
+      alt: "Earrings-Image",
       prouductName: "Earrings",
       price: "$70",
-      proudctStatus:'New'
+      proudctStatus: "New",
     },
     {
       img: product7,
-      alt:"Earrings-Image",
+      alt: "Earrings-Image",
       prouductName: "Earrings",
       price: "$400",
-      proudctStatus:'New'
+      proudctStatus: "New",
     },
     {
       img: product8,
-      alt:"Nacklace-Image",
+      alt: "Nacklace-Image",
       prouductName: "Nacklace",
       price: "$450",
-      proudctStatus:'Old'
+      proudctStatus: "Old",
     },
   ];
   return (
@@ -75,40 +75,38 @@ const Shop = () => {
           <h2 className="text-[32px] font-bold  uppercase ">Latest Products</h2>
         </div>
         <div className="flex justify-center items-center gap-10 flex-wrap relative">
-        {
-          productDetail.map((item)=>(
-           
-          <div
-           className="w-[260px] h-[300px] border-[1px] bg-[#eeeeee] shadow-xl p-10 rounded-lg relative
+          {productDetail.map((item) => (
+            <div
+              className="w-[260px] h-[300px] border-[1px] bg-[#eeeeee] shadow-xl p-10 rounded-lg relative
            cursor-pointer hover:scale-[1.1] duration-[1s]
            
-          ">
-            <div className="bg-white w-14 h-14 rounded-full absolute top-1 left-1 flex justify-center items-center font-bold">
-              {item.proudctStatus}
-            </div>
+          "
+            >
+              <div className="bg-white w-14 h-14 rounded-full absolute top-1 left-1 flex justify-center items-center font-bold">
+                {item.proudctStatus}
+              </div>
 
-            <div className="w-full h-[75%] flex justify-center items-center">
-              <img
-                className="max-w-full max-h-[150px]"
-                src={item.img}
-                alt={item.alt}
-              />
+              <div className="w-full h-[75%] flex justify-center items-center">
+                <img
+                  className="max-w-full max-h-[150px]"
+                  src={item.img}
+                  alt={item.alt}
+                />
+              </div>
+              <div className="h-[20%] flex items-end justify-between ">
+                <p className="text-black text-[16px] font-semibold">
+                  {item.prouductName}
+                </p>
+                <p className="text-black text-[16px]">
+                  Price:
+                  <span className="text-bold text-[#fd9c6b] text-[18px]">
+                    {item.price}
+                  </span>
+                </p>
+              </div>
             </div>
-            <div className="h-[20%] flex items-end justify-between ">
-              <p className="text-black text-[16px] font-semibold">{item.prouductName}</p>
-              <p className="text-black text-[16px]">
-                Price:
-                <span className="text-bold text-[#fd9c6b] text-[18px]">
-                  {item.price}
-                </span>
-              </p>
-            </div>
-          </div>
-          
-      
-          ))
-        }
-          </div>
+          ))}
+        </div>
         <div>
           <Button btnText="View All Products" />
         </div>
