426679d95fb2a65bcf60cc07562caaea190ece06
Basit Ali <opoali911@gmail.com>
2025-01-18 00:18:33 +0500

    the basic structure  of the blog is ready

--- React_Project/HaletProject/src/Pages/Blog/Blog.jsx
+++ React_Project/HaletProject/src/Pages/Blog/Blog.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 const Blog = () => {
   return (
     <>
-          
+    <section>
+      <div>
+        <h1>Latest From Blog</h1>
+      </div>
+      <div>
+        <div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h4>Look even slightly believable. If you are</h4>
+            <p>alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+            <button
+        className="px-10 py-[5px] sm:px-14  sm:py-[10px] text-[#000]  text-lg border-[1px] border-solid border-[#000] hover:text-white hover:bg-[#000] duration-[.5s]"
+        
+      >
+      Read More
+      </button>
+          </div>
+        </div>
+      </div>
+    </section>
+
     </>
   )
 }
