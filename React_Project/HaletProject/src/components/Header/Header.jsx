6b8d3f773c7b341c48c161b4fd4135bab06a03ff
Basit Ali <opoali911@gmail.com>
2025-01-17 14:15:16 +0500

    working on the header component of the healet project

--- React_Project/HaletProject/src/components/Header/Header.jsx
+++ React_Project/HaletProject/src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
+    <>
+    <header className='w-full h-auto bg-[#ABABAB]'>
+      <section className='flex justify-between items-center '>
+        <div>
+          <h1 className=' text-3xl sm:text-5xl text-white font-black'>Healet</h1>
+        </div>
+      <nav className='hidden sm:flex justify-between items-center'>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/shop'>Shop</Link>
+        <Link to='/blog'>Blog</Link>
+      </nav>
+      <div>
+        &#9776;
+      </div>
+      </section>
+      <section></section>
+    </header>
       
-    </div>
+    </>
   )
 }
 
