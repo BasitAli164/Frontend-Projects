06a01854a1b6d669f551753e3d163f58aa398fd5
Basit Ali <opoali911@gmail.com>
2025-01-17 23:45:25 +0500

    the shop page almost complete

--- React_Project/HaletProject/src/Pages/Shop/Shop.jsx
+++ React_Project/HaletProject/src/Pages/Shop/Shop.jsx
@@ -1,76 +1,96 @@
-import React from 'react';
-import Button from '../../components/Button/Button';
-import product1 from '../../assets/images/p1.png'
+import React from "react";
+import Button from "../../components/Button/Button";
+import product1 from "../../assets/images/p1.png";
+import product2 from "../../assets/images/p2.png";
+import product3 from "../../assets/images/p3.png";
+import product4 from "../../assets/images/p4.png";
+import product5 from "../../assets/images/p5.png";
+import product6 from "../../assets/images/p6.png";
+import product7 from "../../assets/images/p7.png";
+import product8 from "../../assets/images/p8.png";
 
 const Shop = () => {
+  const productDetail = [
+    {
+      img: product1,
+      prouductName: "Nacklace",
+      price: "$200",
+    },
+    {
+      img: product2,
+      prouductName: "Nacklace",
+      price: "$300",
+    },
+    {
+      img: product3,
+      prouductName: "Nacklace",
+      price: "$110",
+    },
+    {
+      img: product4,
+      prouductName: "Ring",
+      price: "$45",
+    },
+    {
+      img: product5,
+      prouductName: "Ring",
+      price: "$95",
+    },
+    {
+      img: product6,
+      prouductName: "Earrings",
+      price: "$70",
+    },
+    {
+      img: product1,
+      prouductName: "Earrings",
+      price: "$400",
+    },
+    {
+      img: product1,
+      prouductName: "Nacklace",
+      price: "$450",
+    },
+  ];
   return (
     <>
-    <section className='w-full flex flex-col justify-center items-center mb-20  gap-y-20'>
-      <div className='mt-10'>
-        <h2 className='text-[32px] font-bold  uppercase '>Latest Products</h2>
-      </div>
-     <div className='flex justify-center items-center gap-10 flex-wrap'>
-     <div className='w-[260px] h-[300px] border-[1px] bg-[#eeeeee]'>
-        <div className='w-full h-[75%] flex justify-center items-center'>
-          <img className='max-w-full max-h-[150px]' src={product1} alt="Product-Image" />
+      <section className="w-full flex flex-col justify-center items-center mb-20  gap-y-20">
+        <div className="mt-10">
+          <h2 className="text-[32px] font-bold  uppercase ">Latest Products</h2>
         </div>
-        <div className='h-[20%] flex items-end justify-between '>
-          <p className='text-black text-[16px] font-semibold'>Necklace</p>
-          <p className='text-black text-[16px]'>Price: <span className='text-bold text-[#fd9c6b] text-[18px]'>$200</span></p>
+        <div className="flex justify-center items-center gap-10 flex-wrap">
+        {
+          productDetail.map((item)=>(
+           
+          <div className="w-[260px] h-[300px] border-[1px] bg-[#eeeeee]">
+            <div className="w-full h-[75%] flex justify-center items-center">
+              <img
+                className="max-w-full max-h-[150px]"
+                src={item.img}
+                alt="Product-Image"
+              />
+            </div>
+            <div className="h-[20%] flex items-end justify-between ">
+              <p className="text-black text-[16px] font-semibold">{item.prouductName}</p>
+              <p className="text-black text-[16px]">
+                Price:
+                <span className="text-bold text-[#fd9c6b] text-[18px]">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+          
+      
+          ))
+        }
+          </div>
+        <div>
+          <Button btnText="View All Products" />
         </div>
-      </div>
-     <div className='w-[260px] h-[300px] border-[1px] bg-[#eeeeee]'>
-        <div className='w-full h-[75%] flex justify-center items-center'>
-          <img className='max-w-full max-h-[150px]' src={product1} alt="Product-Image" />
-        </div>
-        <div className='h-[20%] flex items-end justify-between '>
-          <p className='text-black text-[16px] font-semibold'>Necklace</p>
-          <p className='text-black text-[16px]'>Price: <span className='text-bold text-[#fd9c6b] text-[18px]'>$200</span></p>
-        </div>
-      </div>
-     <div className='w-[260px] h-[300px] border-[1px] bg-[#eeeeee]'>
-        <div className='w-full h-[75%] flex justify-center items-center'>
-          <img className='max-w-full max-h-[150px]' src={product1} alt="Product-Image" />
-        </div>
-        <div className='h-[20%] flex items-end justify-between '>
-          <p className='text-black text-[16px] font-semibold'>Necklace</p>
-          <p className='text-black text-[16px]'>Price: <span className='text-bold text-[#fd9c6b] text-[18px]'>$200</span></p>
-        </div>
-      </div>
-     <div className='w-[260px] h-[300px] border-[1px] bg-[#eeeeee]'>
-        <div className='w-full h-[75%] flex justify-center items-center'>
-          <img className='max-w-full max-h-[150px]' src={product1} alt="Product-Image" />
-        </div>
-        <div className='h-[20%] flex items-end justify-between '>
-          <p className='text-black text-[16px] font-semibold'>Necklace</p>
-          <p className='text-black text-[16px]'>Price: <span className='text-bold text-[#fd9c6b] text-[18px]'>$200</span></p>
-        </div>
-      </div>
-     <div className='w-[260px] h-[300px] border-[1px] bg-[#eeeeee]'>
-        <div className='w-full h-[75%] flex justify-center items-center'>
-          <img className='max-w-full max-h-[150px]' src={product1} alt="Product-Image" />
-        </div>
-        <div className='h-[20%] flex items-end justify-between '>
-          <p className='text-black text-[16px] font-semibold'>Necklace</p>
-          <p className='text-black text-[16px]'>Price: <span className='text-bold text-[#fd9c6b] text-[18px]'>$200</span></p>
-        </div>
-      </div>
-     <div className='w-[260px] h-[300px] border-[1px] bg-[#eeeeee]'>
-        <div className='w-full h-[75%] flex justify-center items-center'>
-          <img className='max-w-full max-h-[150px]' src={product1} alt="Product-Image" />
-        </div>
-        <div className='h-[20%] flex items-end justify-between '>
-          <p className='text-black text-[16px] font-semibold'>Necklace</p>
-          <p className='text-black text-[16px]'>Price: <span className='text-bold text-[#fd9c6b] text-[18px]'>$200</span></p>
-        </div>
-      </div>
-     </div>
-      <div>
-        <Button btnText="View All Products"/>
-      </div>
-      </section>      
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
