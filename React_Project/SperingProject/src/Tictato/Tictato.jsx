1d53cdeac1346c84a77724d1c14690c20f14142e
Basit Ali <opoali911@gmail.com>
2025-02-13 22:08:13 +0500

    working with the game project in react

--- React_Project/SperingProject/src/Tictato/Tictato.jsx
+++ React_Project/SperingProject/src/Tictato/Tictato.jsx
@@ -6,7 +6,23 @@ const Tictato = () => {
           Tic Tac Toe Game in
           <span className="text-[#26ffcb] pl-[15px]"> React</span>
         </div>
-        <div className="board"></div>
+        <div className="board">
+          <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+          <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+          </div>
+        </div>
         <button className="w-[250px] h-[75px] border-none outline-none cursor-pointer rounded-full bg-[#1f3540] text-[26px] mt-[25px] mb-[50px] text-[#26ffcb]  ">
           Reset
         </button>
