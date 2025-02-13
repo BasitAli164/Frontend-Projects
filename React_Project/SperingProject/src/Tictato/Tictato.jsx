a4ad0cbabbc9101e78cd969521042c07c12bb4bd
Basit Ali <opoali911@gmail.com>
2025-02-13 21:53:50 +0500

    the basic structure of the game is almost complete only left the borad section or container

--- React_Project/SperingProject/src/Tictato/Tictato.jsx
+++ React_Project/SperingProject/src/Tictato/Tictato.jsx
@@ -2,7 +2,7 @@
 const Tictato = () => {
   return (
     <>
-    <div className="container">
+    <div className="container bg-[#0f1b21] h-screen w-screen ">
       <div className="title">Tic Tac Toe Game in <span>React</span></div>
       <div className="board"></div>
       <button className="reset">Reset</button>
