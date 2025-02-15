867b8d6794f39713a8a0241140e3bff9d33a7d8c
Basit Ali <opoali911@gmail.com>
2025-02-15 22:56:21 +0500

    working on the functional or logic part of the game project

--- React_Project/SperingProject/src/Tictato/Tictato.jsx
+++ React_Project/SperingProject/src/Tictato/Tictato.jsx
@@ -20,6 +20,18 @@ const Tictato = () => {
       setCount(count + 1);
     }
   };
+  const checkWin=()=>{
+    if(data[0]===data[1] && data[1]===data[2]&&data[2]!==''){
+      won(data)
+    }else if(data[3]===data[4] && data[4]===data[5]&&data[5]!==''){
+      won(data)
+    }else if(data[6]===data[7] && data[7]===data[8]&&data[8]!==''){
+      won(data)
+    }
+  }
+  const won=(winner)=>{
+    setLock(true)
+  }
 
   return (
     <>
