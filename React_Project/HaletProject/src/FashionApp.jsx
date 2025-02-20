adc70b5a27da668f556677abe67174b2edc25dce
Basit Ali <opoali911@gmail.com>
2025-02-20 09:33:33 +0500

    make a new project or website of the fashion

---
+++ React_Project/HaletProject/src/FashionApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter,Router,Route} from 'react-router-dom'
+const FashionApp = () => {
+  return (
+   <>
+   </>
+  )
+}
+
+export default FashionApp
--- React_Project/HaletProject/src/main.jsx
+++ React_Project/HaletProject/src/main.jsx
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App.jsx'
 import  './index.css'
 import { StrictMode } from 'react';
+import FashionApp from './FashionApp.jsx';
 
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
-<App />
+{/* <App /> */}
+<FashionApp/>
 
 </StrictMode>
 )
