b1304752a2d3731086d77eca6f66b41b24dde34b
Basit Ali <opoali911@gmail.com>
2025-02-20 13:09:51 +0500

    footer component of the fashion project is almost complete

--- React_Project/HaletProject/src/FashionProject/components/Footer.jsx
+++ React_Project/HaletProject/src/FashionProject/components/Footer.jsx
@@ -16,29 +16,53 @@ const Footer = () => {
     <div className=" bg-black grid grid-cols-3 container p-10 gap-40">
       <div>
         <h1 className="text-3xl  font-semibold text-white">
-          <span className="text-gray-500 " >Little</span> Fashion
+          <span className="text-gray-500 ">Little</span> Fashion
         </h1>
-        <p className="text-gray-500 text-[1.1rem] leading-[26.4px] mt-10">Copyright © 2022 Little Fashion Designed by Tooplate</p>
+        <p className="text-gray-500 text-[1.1rem] leading-[26.4px] mt-10">
+          Copyright © 2022 Little Fashion Designed by Tooplate
+        </p>
       </div>
       <div>
-        <h1>Sitemap</h1>
-        <div>
-          <Link to="/story">Story</Link>
-          <Link to="privacy">Privacy Policy</Link>
-          <Link to="contact">Contact</Link>
-        </div>
-        <div>
-          <Link to="product">Products</Link>
-          <Link to="faqs">FAQs</Link>
+        <h1 className="text-2xl text-white font-semibold">Sitemap</h1>
+        <div className="flex gap-14 mt-5">
+          <div className="text-gray-500 flex flex-col gap-y-2">
+            <Link to="/story">Story</Link>
+            <Link to="privacy">Privacy Policy</Link>
+            <Link to="contact">Contact</Link>
+          </div>
+          <div className="text-gray-500 flex flex-col gap-y-2">
+            <Link to="product">Products</Link>
+            <Link to="faqs">FAQs</Link>
+          </div>
         </div>
       </div>
       <div>
-        <h1>Social</h1>
-        <div>
-          <span>{youTubeIcon}</span>
-          <span>{whatsAppIcon}</span>
-          <span>{instagramIcon}</span>
-          <span>{skypeIcon}</span>
+        <h1 className="text-2xl text-white font-semibold">Social</h1>
+        <div className="flex gap-x-3">
+          <Link
+            to="https://www.youtube.com"
+            className="text-gray-500 font-semibold text-xl"
+          >
+            {youTubeIcon}
+          </Link>
+          <Link
+            to="https://www.whatsapp.com"
+            className="text-gray-500 font-semibold text-xl"
+          >
+            {whatsAppIcon}
+          </Link>
+          <Link
+            to="https://www.instagram.com"
+            className="text-gray-500 font-semibold text-xl"
+          >
+            {instagramIcon}
+          </Link>
+          <Link
+            to="https://www.skye.com"
+            className="text-gray-500 font-semibold text-xl"
+          >
+            {skypeIcon}
+          </Link>
         </div>
       </div>
     </div>
