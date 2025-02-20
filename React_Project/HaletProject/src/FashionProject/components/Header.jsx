582e35fd0b2554f4e0e2818e4046f3989d7c7538
Basit Ali <opoali911@gmail.com>
2025-02-20 10:55:58 +0500

    the header component is complete

--- React_Project/HaletProject/src/FashionProject/components/Header.jsx
+++ React_Project/HaletProject/src/FashionProject/components/Header.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUser,faShoppingBag} from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
+  const userIcon=<FontAwesomeIcon icon={faUser}/>
+  const shopingBag=<FontAwesomeIcon icon={faShoppingBag}/>
+  const [isHome,setIsHome]=useState(true)
   return (
     <>
-    <div className='flex justify-evenly items-center w-full h-20 bg-black'>
+    <div className='container  px-10 flex justify-between items-center w-full h-20 bg-black'>
     <div>
-        <h1 className='text-white text-2xl font-semibold'><span className='text-orange-500'>Little</span> Fashion</h1>
+        <h1 className='text-white text-2xl font-semibold'><span className='text-orange-400'>Little</span> Fashion</h1>
     </div>
     <div>
       <nav >
         <ul className='flex gap-x-5'>
-          <li><Link to='/' className=' font-semibold focus:text-orange-500 focus:underline focus:decoration-gray-600 focus:decoration-[2px]'>Home</Link></li>
-          <li><Link to='/story' className='font-semibold  focus:text-orange-500 focus:underline focus:decoration-gray-600 focus:decoration-[2px]'>Story</Link></li>
-          <li><Link to='/products' className='font-semibold  focus:text-orange-500 focus:underline focus:decoration-gray-600 focus:decoration-[2px]'>Products</Link></li>
-          <li><Link to='/faqs' className='font-semibold  focus:text-orange-500 focus:underline focus:decoration-gray-600 focus:decoration-[2px]'>FAQs</Link></li>
-          <li><Link to='/contact' className='font-semibold  focus:text-orange-500 focus:underline focus:decoration-gray-600 focus:decoration-[2px] hover:text-orange-500 hover:underline hover:decoration-[2px] hover:decoration-gray-500  hover:transition hover:duration-[0.4s]'>Contact</Link></li>
+          <li><Link to='/' className={`${isHome?'text-orange-400 underline decoration-gray-500 decoration-[2px] font-semibold  ':'font-semibold focus:text-orange-400 focus:underline focus:decoration-gray-600 focus:decoration-[2px] hover:text-orange-400 hover:underline hover:decoration-[2px] hover:decoration-gray-500  hover:transition hover:duration-[0.4s]'}`}>Home</Link></li>
+          <li><Link to='/story' onClick={()=>setIsHome(false)} className='font-semibold  focus:text-orange-400 focus:underline focus:decoration-gray-600 focus:decoration-[2px] hover:text-orange-400 hover:underline hover:decoration-[2px] hover:decoration-gray-500  hover:transition hover:duration-[0.4s]'>Story</Link></li>
+          <li><Link to='/products' onClick={()=>setIsHome(false)} className='font-semibold  focus:text-orange-400 focus:underline focus:decoration-gray-600 focus:decoration-[2px] hover:text-orange-400 hover:underline hover:decoration-[2px] hover:decoration-gray-500  hover:transition hover:duration-[0.4s]'>Products</Link></li>
+          <li><Link to='/faqs' onClick={()=>setIsHome(false)} className='font-semibold  focus:text-orange-400 focus:underline focus:decoration-gray-600 focus:decoration-[2px] hover:text-orange-400 hover:underline hover:decoration-[2px] hover:decoration-gray-500  hover:transition hover:duration-[0.4s]'>FAQs</Link></li>
+          <li><Link to='/contact' onClick={()=>setIsHome(false)} className='font-semibold  focus:text-orange-400 focus:underline focus:decoration-gray-600 focus:decoration-[2px] hover:text-orange-400 hover:underline hover:decoration-[2px] hover:decoration-gray-500  hover:transition hover:duration-[0.4s]'>Contact</Link></li>
         </ul>
       </nav>
 
     </div>
-    <div></div>
+    <div className='flex gap-x-5'>
+      <div className='hover:text-orange-400 cursor-pointer'>{userIcon}</div>
+      <div className='hover:text-orange-400 cursor-pointer'>{shopingBag}</div>
+    </div>
       
     </div>
     </>
