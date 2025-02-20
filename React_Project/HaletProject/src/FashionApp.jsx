83908e6bf8fef7153b93574fab09b6e613d319b8
Basit Ali <opoali911@gmail.com>
2025-02-20 09:54:45 +0500

    working  in the fashion project

--- React_Project/HaletProject/src/FashionApp.jsx
+++ React_Project/HaletProject/src/FashionApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
-import Home from '../FashionProject/pages/Home'
+import Header from './FashionProject/components/Header'
+import Home from './FashionProject/pages/Home'
 const FashionApp = () => {
   return (
    <>
    <Router>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes>
---
+++ React_Project/HaletProject/src/FashionProject/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className='flex justify-evenly items-center w-full h-20 bg-black'>
+    <div>
+        <h1 className='text-white text-2xl font-semibold'><span className='text-orange-500'>Little</span> Fashion</h1>
+    </div>
+    <div>
+      
+    </div>
+    <div></div>
+      
+    </div>
+    </>
+  )
+}
+
+export default Header
