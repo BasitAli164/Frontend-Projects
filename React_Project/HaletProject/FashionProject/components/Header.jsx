35cf2eecf650e32fd4e06b99874e2c513d6166cf
Basit Ali <opoali911@gmail.com>
2025-02-20 09:41:17 +0500

    exchange the image directory

---
+++ React_Project/HaletProject/FashionProject/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div>
+        <h1 className=''><span>Little</span> Fashion</h1>
+    </div>
+    <div></div>
+    <div></div>
+      
+    </>
+  )
+}
+
+export default Header
--- React_Project/HaletProject/src/FashionApp.jsx
+++ React_Project/HaletProject/src/FashionApp.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import {BrowserRouter,Router,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Home from '../FashionProject/pages/Home'
 const FashionApp = () => {
   return (
    <>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+   </Router>
    </>
   )
 }
